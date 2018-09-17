d8c9c082c44fc3fc4ceab886704821fc1b0bb700
Jinyoung Eom <jinyoung.eom@gmail.com>
2018-09-18 01:08:37 +0900

    .

---
+++ eomcs-java-netty/src/main/java/com/eomcs/netty/ex01/DiscardClient.java
@@ -0,0 +1,33 @@
+package com.eomcs.netty.ex01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DiscardClient {
+    public static void main(String[] args) throws Exception {
+
+        Scanner keyIn = new Scanner(System.in);
+        try (
+            Socket socket = new Socket("localhost", 8888);
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(
+                 new InputStreamReader(socket.getInputStream()));) {
+            
+            while (true) {
+                System.out.print("입력> ");
+                String input = keyIn.nextLine();
+                if (input.equals("exit"))
+                    break;
+                out.println(input);
+                out.flush();
+                
+                System.out.println(in.readLine());
+            }
+        }
+        keyIn.close();
+
+    }
+}
---
+++ eomcs-java-netty/src/main/java/com/eomcs/netty/ex01/DiscardServerHandler.java
@@ -0,0 +1,37 @@
+package com.eomcs.netty.ex01;
+
+import java.nio.charset.Charset;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        System.out.println("DiscardServerHandler.channelRead()");
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            /*
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                //System.out.flush();
+            }
+            */
+            System.out.println(in.readCharSequence(in.capacity(), Charset.forName("UTF-8")));
+            in.resetReaderIndex();
+            ctx.writeAndFlush(msg);
+            
+        } finally {
+            //in.release(); // (2)
+        }
+    }
+    
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        System.out.println("DiscardServerHandler.exceptionCaught()");
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
