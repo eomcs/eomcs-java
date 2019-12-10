7e15a874a11ea96f79ac5db52fd8b3cd7fe32eb0
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-10 16:32:18 +0900

    .

--- src/main/java/com/eomcs/basic/ex03/Exam7.java
+++ src/main/java/com/eomcs/basic/ex03/Exam7.java
@@ -1,7 +1,7 @@
 package com.eomcs.basic.ex03;
 
 //# 이스케이프 문자(escape character) = 문자 제어 코드
-//- 화면에 출력하는 문자가 아니라 문자 출력을 제어하는 명령이다.
+//- 화면에 출력하는 문자가 아니라 문자 출력을 제어하는 문자이다.
 //- 제어 문자
 //  \n - Line Feed(LF), 0x0a
 //  \r - Carrage Return(CR), 0x0d
@@ -14,16 +14,19 @@ package com.eomcs.basic.ex03;
 //
 public class Exam7 {
   public static void main(String[] args) {
-    System.out.println("Hello,\nworld!");  // 줄바꿈은 이 제어 문자를 사용하라!
-    System.out.println("Hello,\rabc");
-    System.out.println("Hello,\b\b\bworld!");
-    System.out.println("Hello,\tworld!");
-    System.out.println("Hello,\fworld!");
-    System.out.println("Hello,\"w\"orld!");
-    System.out.println("Hello,'w'orld!");
-    System.out.println('\'');
-    System.out.println('"');
-    System.out.println("Hello,\\\\\\world!");
+    System.out.println("Hello,world!"); 
+    System.out.println("Hello,\nworld!"); // 줄바꿈 문자
+    System.out.println("Hello,\rabc"); // 커서(cursor)를 처음으로 돌리는 문자
+    // 커서란? 문자를 출력할 위치를 가리키는 것.
+
+    System.out.println("Hello,\b\b\bworld!"); // 커서를 뒤로 한 칸 이동시키는 문자
+    System.out.println("Hello,\tworld!"); // 탭 공간을 추가시키는 문자
+    System.out.println("Hello,\fworld!"); // 
+    System.out.println("Hello,\"w\"orld!"); // " 문자를 출력시키는 문자
+    System.out.println("Hello,'w'orld!"); //"" 안에서 ' 문자는 그냥 적는다.
+    System.out.println('\''); // ' 문자를 출력시키는 문자
+    System.out.println('"'); // '' 안에서 " 문자는 그냥 적는다.
+    System.out.println("c:\\Users\\user\\git"); // \ 문자를 출력시키는 문자
   }
 }
 
