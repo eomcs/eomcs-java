8906745a9ef03d9fb9fb53942da92144dcbdabd8
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-12 17:49:00 +0900

    소스정리

---
+++ src/main/java/com/eomcs/basic/ex04/assignment2/Test02.java
@@ -0,0 +1,18 @@
+// 과제 1 : 가위,바위,보 게임 애플리케이션을 작성하라.
+// - 실행1
+// 가위,바위,보? 보
+// 컴퓨터: 가위  
+// => 졌습니다.
+//
+// - 실행2
+// 가위,바위,보? 바위
+// 컴퓨터: 가위
+// => 이겼습니다.
+//
+package com.eomcs.basic.ex04.assignment;
+
+public class Test02 {
+  public static void main(String[] args) {
+
+  }
+}
