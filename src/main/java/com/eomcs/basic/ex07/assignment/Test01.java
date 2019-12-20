f2ac8afcef39c5b6bc429ae93e1f0e034c8ee0ba
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-20 17:29:47 +0900

    소스정리

---
+++ src/main/java/com/eomcs/basic/ex07/assignment/Test01.java
@@ -0,0 +1,17 @@
+package com.eomcs.basic.ex07.assignment;
+
+public class Test01 {
+  public static void main(String[] args) {
+    // 배열의 값 중에서 최대 값을 출력하라.
+    int[] values = {34, 4, -3, 78, 12, 22, 45, 0, -22};
+    int value = max(values);
+    System.out.println(value);
+    // 출력결과:
+    // 78
+  }
+  
+  static int max(int[] values) {
+    // 파라미터로 배열을 받고, 그 값 중에서 최대 값을 찾아 리턴한다.
+    return 0;
+  }
+}
