9833ebe50609467a799f47059a0c07462a3afd9c
eomjinyoung <jinyoung.eom@gmail.com>
2019-12-23 17:12:48 +0900

    2019-12-23

---
+++ src/main/java/com/eomcs/basic/ex07/assignment/Test04.java
@@ -0,0 +1,28 @@
+package com.eomcs.basic.ex07.assignment;
+
+// 과제: 재귀호출을 이용하여 n! 을 계산하라.
+// 실행)
+// 입력? 5
+// 5! = 1 * 2 * 3 * 4 * 5 = 120
+//
+public class Test04 {
+  public static void main(String[] args) {
+    // 사용자로부터 정수 값을 입력 받는다.
+    int n = 0;
+    
+    // n! 을 계산한다.
+    int result = factorial(n);
+    
+    // 결과 값을 출력한다.
+  }
+  
+  static int factorial(int n) {
+    // 코드를 완성하시오!
+    return 0;
+  }
+}
+
+
+
+
+
---
+++ src/main/java/com/eomcs/basic/ex07/assignment/Test05.java
@@ -0,0 +1,30 @@
+package com.eomcs.basic.ex07.assignment;
+
+// 과제: 재귀호출을 이용하여 직삼각형을 출력하라.
+// 실행)
+// 밑변의 길이? 5
+// *
+// **
+// ***
+// ****
+// *****
+//
+public class Test05 {
+  public static void main(String[] args) {
+    // 사용자로부터 밑변의 길이를 입력 받는다.
+    int base = 0;
+    
+    
+    // 직삼각형을 출력한다.
+    printTriangle(base);
+  }
+  
+  static void printTriangle(int base) {
+    // 코드를 완성하시오!
+  }
+}
+
+
+
+
+
