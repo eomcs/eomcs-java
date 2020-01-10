61daf63720343c368a97caa4d2dae6f9360f7e18
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-10 15:37:02 +0900

    소스정리

---
+++ src/main/java/com/eomcs/generic/ex01/Exam0213.java
@@ -0,0 +1,55 @@
+// 제네릭(Generic) 문법 정리 - 레퍼런스와 인스턴스 생성 
+package com.eomcs.generic.ex01;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import com.eomcs.generic.ex01.Exam0220.A;
+import com.eomcs.generic.ex01.Exam0220.B1;
+
+public class Exam0213 {
+  
+  static class A {}
+  static class B1 extends A {}
+  static class B2 extends A {}
+  static class C extends B1 {}
+  /*
+   *   Object
+   *     |
+   *     A
+   *    / \
+   *   B1 B2
+   *   |
+   *   C
+   */
+  
+  public static void main(String[] args) {
+    
+    ArrayList<A> list1;
+    
+    list1 = new ArrayList(); // 이렇게 사용하지 말고, 명확히 제네릭의 타입을 지정하라.
+    //list1 = new ArrayList<Object>(); // 컴파일 오류!
+    list1 = new ArrayList<>();
+    list1 = new ArrayList<A>();
+    //list1 = new ArrayList<B1>(); // 컴파일 오류!
+    //list1 = new ArrayList<B2>(); // 컴파일 오류!
+    //list1 = new ArrayList<C>(); // 컴파일 오류!
+    
+    ArrayList<B2> list2;
+    
+    list2 = new ArrayList(); // 이렇게 사용하지 말고, 명확히 제네릭의 타입을 지정하라.
+    //list2 = new ArrayList<Object>(); // 컴파일 오류!
+    list2 = new ArrayList<>();
+    //list2 = new ArrayList<A>(); // 컴파일 오류!
+    //list2 = new ArrayList<B1>(); // 컴파일 오류!
+    list2 = new ArrayList<B2>();
+    //list2 = new ArrayList<C>(); // 컴파일 오류!
+  }
+}
+
+
+
+
+
+
+
+
