70d230467b24da0e3bf688f452a7c72f052443fd
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-13 14:25:53 +0900

    소스정리

--- src/main/java/com/eomcs/algorithm/data_structure/stack/StackTest.java
+++ src/main/java/com/eomcs/algorithm/data_structure/stack/StackTest.java
@@ -1,15 +1,19 @@
-package com.eomcs.algorithm.data_structure.stack.step3;
+package com.eomcs.algorithm.data_structure.stack;
 
 public class StackTest {
+
   public static void main(String[] args) {
-    Stack<String> stack = new Stack<>();
+    
+    Stack stack = new Stack();
+    
     stack.push("aaa");
-    stack.push("bbb"); 
+    stack.push("bbb");
     stack.push("ccc");
     stack.push("ddd");
-     
+    
     while (!stack.empty()) {
       System.out.println(stack.pop());
     }
   }
+
 }
