147aca901534ef7c2cea1b768a67d5c83d73697d
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-16 16:38:31 +0900

    소스정리

--- src/main/java/com/eomcs/oop/ex11/a/Exam0113.java
+++ src/main/java/com/eomcs/oop/ex11/a/Exam0113.java
@@ -1,12 +1,14 @@
 // static nested class
 package com.eomcs.oop.ex11.a;
 
-// 다음과 같이 특정 클래스의 스태틱 멤버를 미리 선언해 두면 
-// 그 멤버를 사용할 때 일일이 클래스 이름을 작성할 필요가 없다.
+// 다음과 같이 import static 문법을 이용하여 
+// 특정 클래스의 스태틱 멤버를 미리 선언해 둘 수 있다. 
+// 그러면 멤버를 사용할 때 일일이 클래스 이름을 지정할 필요가 없다.
 //import static com.eomcs.oop.ex11.a.Exam0112_X.sValue;
 //import static com.eomcs.oop.ex11.a.Exam0112_X.A;
 
-// 물론 다음과 같이 각 멤버를 지정하지 않고 wildcard(*)로 전체 멤버를 지정할 수 있다.
+// 각각 멤버를 지정하는 대신 
+// 다음과 같이 wildcard(*)를 사용하여 전체 멤버를 한 번에 지정할 수 있다.
 import static com.eomcs.oop.ex11.a.Exam0112_X.*;
 
 public class Exam0113 {
