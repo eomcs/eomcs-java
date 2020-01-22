45daab4a9851ef5bf86f344580d5a92abbf2386d
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-22 09:24:10 +0900

    소스정리

--- src/main/java/com/eomcs/basic/ex06/Exam0433.java
+++ src/main/java/com/eomcs/basic/ex06/Exam0433.java
@@ -2,11 +2,13 @@ package com.eomcs.basic.ex06;
 
 // # 흐름 제어문 - for 중첩과 break
 //
-public class Exam0430 {
+public class Exam0433 {
   public static void main(String[] args) {
-    // for 문의 중첩
+    // continue
     for (int i = 1; i <= 10; i++) {
       for (int j = 1; j <= i; j++) {
+        if (j % 2 == 0)
+          continue; // 다음 줄로 가지 않고 '변수증가문'으로 이동한다.
         System.out.print(j + " ");
       }
       System.out.println();
