34107e343f1ec51064542f13b2ef2f9e1ddfffa4
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-28 17:40:17 +0900

    소스정리

--- src/main/java/com/eomcs/io/ex03/Exam0210.java
+++ src/main/java/com/eomcs/io/ex03/Exam0210.java
@@ -1,4 +1,4 @@
-// Character Stream - 문자 배열 출력하기 
+// Character Stream - 문자 배열 출력하기
 package com.eomcs.io.ex03;
 
 import java.io.FileWriter;
@@ -8,14 +8,22 @@ public class Exam0210 {
   public static void main(String[] args) throws Exception {
     FileWriter out = new FileWriter("temp/test2.txt");
 
-    char[] chars = new char[] {'A','B','C','가','각','간','똘','똥'}; 
+    char[] chars = new char[] {'A', 'B', 'C', '가', '각', '간', '똘', '똥'};
 
     out.write(chars); // 문자 배열 전체를 출력한다.
-    // 당연히 UTF-16을 JVM 기본 문자 코드표에 따라 변환하여 출력한다.
+    // 당연히 UCS2를 JVM 기본 문자 코드표에 따라 변환하여 출력한다.
     // JVM이 입출력 문자 코드표로 UTF-8을 사용한다면
     // 영어는 1바이트로 변환되어 출력될 것이고,
     // 한글은 3바이트로 변환되어 출력될 것이다.
-
+    // JVM(UCS2)    File(UTF-8)
+    // 00 41    ==> 41
+    // 00 42    ==> 42
+    // 00 43    ==> 43
+    // ac 00    ==> ea b0 80
+    // ac 01    ==> ea b0 81
+    // ac 04    ==> ea b0 84
+    // b6 18    ==> eb 98 98
+    // b6 25    ==> eb 98 a5
     out.close();
 
     System.out.println("데이터 출력 완료!");
--- src/main/java/com/eomcs/io/ex03/Exam0320.java
+++ src/main/java/com/eomcs/io/ex03/Exam0320.java
@@ -1,4 +1,4 @@
-// Character Stream - 읽은 데이터를 문자 배열의 특정 위치에 저장하기 
+// Character Stream - 읽은 데이터를 문자 배열의 특정 위치에 저장하기
 package com.eomcs.io.ex03;
 
 import java.io.FileReader;
@@ -12,20 +12,19 @@ public class Exam0320 {
 
     // read(버퍼의주소, 저장할위치, 읽을바이트개수)
     // => 리턴 값은 실제 읽은 문자의 개수이다.
-    int count = in.read(buf, 10, 40); // 40개의 문자를 읽어 10번 방부터 저장한다. 
+    int count = in.read(buf, 10, 40); // 40개의 문자를 읽어 10번 방부터 저장한다.
 
     in.close();
 
     System.out.printf("%d\n", count);
-    for (int i = 10; i < (count + 10); i++)
-      System.out.printf("%c(%x) ", buf[i], (int)buf[i]);
+
+    for (int i = 0; i < 20; i++)
+      System.out.printf("%c(%x) ", buf[i], (int) buf[i]);
 
     System.out.println();
 
   }
 
 }
-
-
 
 
