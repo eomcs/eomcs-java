7aafce32cc09ec9433568545e2d8c7b02dde12c5
eomjinyoung <jinyoung.eom@gmail.com>
2020-01-30 11:59:05 +0900

    소스정리

--- src/main/java/com/eomcs/io/ex07/Exam0220.java
+++ src/main/java/com/eomcs/io/ex07/Exam0220.java
@@ -23,6 +23,16 @@ public class Exam0220 {
     in.close();
 
     System.out.printf("%s\n", member);
+    
+    // 문제?
+    // => DataBufferedInputStream과 DataInputStream 클래스는
+    //    생성자를 빼고 나머지 코드가 모두 같다.
+    // => 안타깝게도 DataInputStream의 코드를 복사하지 않고
+    //   재사용하는 방법이 없다.
+    //   이것이 상속으로 기능을 확장했을 때의 한계이다.
+    // => 해결책?
+    //   바이트 값을 읽어 String,int,boolean 값으로 바꾸는 코드를
+    //   장신구(decorator)처럼 붙였다 뗐다 하게 만들라!
   }
 }
 
---
+++ src/main/java/com/eomcs/io/ex08/DecoratorInputStream.java
@@ -0,0 +1,21 @@
+package com.eomcs.io.ex08;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public abstract class DecoratorInputStream extends InputStream {
+  InputStream 연결된부품;
+
+  public DecoratorInputStream(InputStream 부품) {
+    // 장식품처럼 붙였다 뗐다 할 수 있는 기능을 수행하는 객체는
+    // 다른 장식품과 연결할 수 있도록 생성자에서 그 객체의 주소를 받아야 한다.
+    this.연결된부품 = 부품;
+  }
+
+  @Override
+  public int read() throws IOException {
+    // read() 메서드가 호출되면,
+    // 이 장식품과 연결된 다른 부품의 read()를 실행한다.
+    return 연결된부품.read();
+  }
+}
