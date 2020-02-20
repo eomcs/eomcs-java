a06c4f0bc15b5f865518c3fdc29b231df3bfa454
eomjinyoung <jinyoung.eom@gmail.com>
2020-02-20 15:09:42 +0900

    소스정리

--- src/main/java/com/eomcs/jdbc/ex1/Exam0210.java
+++ src/main/java/com/eomcs/jdbc/ex1/Exam0210.java
@@ -11,28 +11,50 @@ public class Exam0210 {
     try {
       // DBMS에 연결하기
       // => DriverManager에게 DBMS와의 연결을 요청한다. 어느 서버에 접속할 것인지 정보를 제공해야 한다.
-      // jdbc url : DBMS 서버 정보. 예) jdbc:mysql://서버주소:포트/데이터베이스명
+      // jdbc url : DBMS 서버 정보. 예) jdbc:DBMS://서버주소:포트/데이터베이스명
       // (포트번호를 지정하지 않으면 기본이 3306 이다.)
       // username : DBMS 사용자 아이디
       // password : DBMS 사용자 암호
       //
       con = DriverManager.getConnection( //
-          "jdbc:mysql://localhost:3306/studydb", // jdbcURL
+          "jdbc:mariadb://localhost:3306/studydb", // jdbcURL
           "study", // username
           "1111" // password
       );
-      // => DriverManager는 등록된 java.sql.Driver 구현체 중에서 jdbc url에 지정된 객체를 찾는다.
+      // => DriverManager는 등록된 java.sql.Driver 구현체 중에서
+      // jdbc url에 지정된 Driver 객체를 찾는다.
       // 예) MariaDB: org.mariadb.jdbc.Driver 클래스의 객체
       // => DB 연결을 Driver 구현체에게 위임한다.
+      // 즉, Driver 객체의 connect()를 호출한다.
       // => Driver 구현체(org.mariadb.jdbc.Driver 객체)는 DBMS와 연결한 후
       // 소켓 정보를 갖고 있는 java.sql.Connection 구현체를 리턴한다.
       //
+      // [App]...................[DriverManager].............[Driver 구현체]
+      // ..| getConnection()............|............................|
+      // ..|--------------------------->|............................|
+      // ..|............................|......connect().............|
+      // ..|............................|--------------------------->|
+      // ..|............................|............................|-> new Connection()
+      // ..|............................|..........return............|
+      // ..|........return..............|<---------------------------|
+      // ..|<---------------------------|............................|
+      //
       System.out.println("DBMS와 연결됨!");
+
+      // MariaDB의 Driver 구현체가 리턴한 Connection 객체는
+      // 어떤 클래스일까?
+      System.out.println(con.getClass().getName());
 
     } finally {
       // 자원해제
       // => 파일과 마찬가지로 DBMS에 연결한 후 더이상 사용하지 않으면 연결을 해제해야 한다.
-      con.close();
+      try {
+        con.close();
+      } catch (Exception e) {
+        // 연결 해제하다가 발생된 예외는 무시한다.
+        // 왜? 이런 오류는 애플리케이션에서 처리할 방법이 없다.
+        // 처리할 필요도 없다.
+      }
       System.out.println("DBMS와 연결 해제됨!");
     }
   }
--- src/main/java/com/eomcs/jdbc/ex1/Exam0310.java
+++ src/main/java/com/eomcs/jdbc/ex1/Exam0310.java
@@ -7,13 +7,15 @@ public class Exam0310 {
 
   public static void main(String[] args) throws Exception {
 
-    try (
-        java.sql.Connection con = DriverManager
-            .getConnection("jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
+    try (java.sql.Connection con = DriverManager.getConnection( //
+        "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
 
         // java.sql.Statement 구현 객체를 얻는다.
         // - SQL문을 DBMS의 형식에 따라 인코딩하여 서버에 전달하는 일을 하는 객체.
         java.sql.Statement stmt = con.createStatement();) {
+
+      // MariaDB의 Connection 객체가 리턴하는 Statement 구현체의 클래스 이름은?
+      System.out.println(stmt.getClass().getName());
 
       // Statement 객체 사용법:
       //
@@ -31,7 +33,8 @@ public class Exam0310 {
       // "DQL(Data Query Language)"
       // => select처럼 data를 조회하는 sql 명령을 말한다.
       //
-      int count = stmt.executeUpdate("insert into x_board(title,contents) values('제목10','내용')");
+      int count = stmt.executeUpdate( //
+          "insert into x_board(title,contents) values('제목10','내용')");
       System.out.printf("%d 개 입력 성공!", count);
     }
   }
