0d33db7e1d1bd8f860a1886e4661c4074a096ebd
eomjinyoung <jinyoung.eom@gmail.com>
2020-02-28 16:43:22 +0900

    소스정리

--- src/main/java/com/eomcs/mybatis/ex02/Exam0221.java
+++ src/main/java/com/eomcs/mybatis/ex02/Exam0221.java
@@ -1,4 +1,4 @@
-// Mybatis - SQL에 파라미터 지정하기 : #{} 의 한계
+// Mybatis - SQL에 파라미터 지정하기 : ${}의 쓰임새
 package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
@@ -8,7 +8,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0220 {
+public class Exam0221 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream = Resources.getResourceAsStream(//
@@ -21,13 +21,8 @@ public class Exam0220 {
     // 예) 파라미터로 컬럼 이름을 넘겨주면
     // 해당 컬럼의 값을 오름차순으로 정렬한다.
     List<Board> list = sqlSession.selectList(//
-        "BoardMapper.selectBoard1", "title");
-    // => 파라미터 값을 SQL에 그대로 삽입하려면
-    // #{} 문법을 사용해서는 안된다.
-    // ${} 문법을 사용해야 한다.
-    // => #{}은 값을 넣을 때 사용한다.
-    // => 현재 selectBoard1에서는 #{}을 사용했기 때문에
-    // order by가 정상적으로 적용되지 못했다.
+        "BoardMapper.selectBoard2", "title");
+    // ${} 문법은 값을 SQL 문에 그대로 삽입한다.
 
     for (Board board : list) {
       System.out.printf("%d, %s, %s, %s\n", //
