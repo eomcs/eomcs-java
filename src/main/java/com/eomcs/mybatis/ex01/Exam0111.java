15efe628d4d3d76aeb17caa981f6d1067d015b19
eomjinyoung <jinyoung.eom@gmail.com>
2020-02-28 12:24:06 +0900

    2020-02-28

--- src/main/java/com/eomcs/mybatis/ex01/Exam0111.java
+++ src/main/java/com/eomcs/mybatis/ex01/Exam0111.java
@@ -1,8 +1,8 @@
 // Data Persistence Framework 도입 - Mybatis
 package com.eomcs.mybatis.ex01;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -34,37 +34,23 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 // - BoardMapper.xml 생성 및 편집
 // 5) Mybatis 객체 준비
 //
-public class Exam0110 {
+public class Exam0111 {
 
   public static void main(String[] args) throws Exception {
-    // 1. mybatis 설정 파일을 읽을 InputStream 도구를 준비한다.
-    // 1) 직접 파일 시스템 경로를 지정하기
-    // => 단 소스 파일 경로를 지정하지 말아라.
-    // => 컴파일된 후 XML 파일이 놓이는 경로를 지정해라.
-    // => 자바 패키지에 작성한 일반 파일은 그대로 빌드 디렉토리에 복사된다.
-    // => 예) 프로젝트폴더/bin/main/com/eomcs/mybatis/ex01/mybatis-config.xml
-    //
-    InputStream mybatisConfigInputStream = new FileInputStream(//
-        "./bin/main/com/eomcs/mybatis/ex01/mybatis-config.xml");
+    // mybatis 설정 파일을 읽을 InputStream 도구를 준비한다.
+    // => Resources의 getResourceAsStream() 메서드는
+    // 파라미터에 지정한 파일 경로를 CLASSPATH에서 찾는다.
+    // 그리고 그 파일을 읽을 수 있는 InputStream 도구를 리턴한다.
+    // => 개발자가 mybatis-config.xml 파일의 전체 경로를 지정하지 않아도 되기 때문에
+    // 매우 편리하다.
+    // => 단 해당 파일은 CLASSPATH에 있어야 한다.
+    InputStream inputStream = Resources.getResourceAsStream("step25/ex5/mybatis-config.xml");
 
-    // 2. SqlSessionFactory를 만들어 줄 빌더 객체 준비
-    //
-    SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
+    // mybatis 설정 파일을 가지고 Builder를 이용하여
+    // SqlSession 공장 객체를 생성한다.
+    SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
 
-    // 3. SqlSession 객체를 만들어 줄 팩토리 객체 준비
-    // => mybatis는 Builder를 이용하여 SqlSessionFactory 객체를 만든다.
-    // => 이때 공장 객체를 만들 때 사용할 설정 파일을 지정한다.
-    // => 설정 파일의 경로를 직접 지정하지 말고,
-    // 해당 파일을 읽을 때 사용할 InputStream을 넘겨줘라.
-    //
-    SqlSessionFactory factory = factoryBuilder.build(mybatisConfigInputStream);
-
-    // 4. SQL을 실행시키는 객체 준비
-    // => SqlSessionFactory 객체로부터 SqlSession 객체를 얻는다.
-    // => openSession()은 수동 커밋으로 SQL을 다루는 객체를 리턴한다.
-    // => 자동 커밋으로 SQL을 다루고 싶다면,
-    // openSession(boolean autoCommit) 메서드를 호출하라.
-    //
+    // SqlSession 공작 객체로부터 SqlSession 객체를 얻는다.
     SqlSession sqlSession = factory.openSession();
 
     System.out.println("mybatis 준비 완료!");
