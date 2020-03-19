51a2827a8a0f0a9b8395a64992dca3fe1fc9b9de
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-19 11:34:44 +0900

    소스정리

---
+++ src/main/java/com/eomcs/spring/ioc/ex02/a/Exam01.java
@@ -0,0 +1,19 @@
+// 객체 생성 - <bean> 태그 사용법
+package com.eomcs.spring.ioc.ex02.a;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.eomcs.spring.ioc.SpringUtils;
+
+public class Exam01 {
+
+  public static void main(String[] args) {
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(//
+        "com/eomcs/spring/ioc/ex02/a/application-context.xml");
+
+    SpringUtils.printBeanList(iocContainer);
+  }
+
+}
+
+
--- src/main/java/com/eomcs/spring/ioc/ex02/b/Exam01.java
+++ src/main/java/com/eomcs/spring/ioc/ex02/b/Exam01.java
@@ -1,23 +1,20 @@
-// 객체 생성 
-package com.eomcs.spring.ioc.ex02;
+// 객체 생성 - bean의 이름을 지정하는 다양한 방법
+package com.eomcs.spring.ioc.ex02.b;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.eomcs.spring.ioc.SpringUtils;
 
 public class Exam01 {
 
   public static void main(String[] args) {
-    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(
-        "bitcamp/java106/step02/application-context-01.xml");
+    ApplicationContext iocContainer = new ClassPathXmlApplicationContext(//
+        "com/eomcs/spring/ioc/ex02/b/application-context.xml");
 
+    // 빈의 id와 클래스명을 출력하기
     SpringUtils.printBeanList(iocContainer);
   }
 
 }
-
-
-
 
 
