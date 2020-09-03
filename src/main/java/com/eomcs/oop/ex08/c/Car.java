a6aacb8ac95d1be6f95cbbbd5fb416719309b5f3
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-03 12:53:20 +0900

    소스정리

--- src/main/java/com/eomcs/oop/ex08/c/Car.java
+++ src/main/java/com/eomcs/oop/ex08/c/Car.java
@@ -4,6 +4,7 @@ package com.eomcs.oop.ex08.c;
 // => 서브클래스에게 공통 기능을 상속해주는 목적으로 만든 클래스이다.
 // => 직접 사용하지 않는 클래스이다.
 // => 즉 개발자에게 이 클래스를 상속 받아 새 클래스를 만들어 쓰라는 의미다!
+// => 보통 '일반화(generalization)' 과정에서 생성되는 클래스를 추상 클래스로 만든다.
 public abstract class Car {
 
   public Car() {
