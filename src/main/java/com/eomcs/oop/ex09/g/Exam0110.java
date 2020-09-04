032e746696f8f416ee4aa230791dcc014678be20
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-04 14:42:59 +0900

    소스정리

--- src/main/java/com/eomcs/oop/ex09/g/Exam0110.java
+++ src/main/java/com/eomcs/oop/ex09/g/Exam0110.java
@@ -1,7 +1,7 @@
 // 인터페이스 구현 - 추상 메서드 구현
 package com.eomcs.oop.ex09.g;
 
-public class Exam0110 implements A {
+public class Exam0110 extends B implements A {
   @Override
   public void m3() {
     // 추상 메서드는 반드시 구현해야 한다.
