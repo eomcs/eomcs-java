984df8cbdbeb6f3bd9fcc615faef8cf3d0edd068
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-15 15:41:10 +0900

    소스정리

--- src/main/java/com/eomcs/io/ex01/Exam0431.java
+++ src/main/java/com/eomcs/io/ex01/Exam0431.java
@@ -10,7 +10,8 @@ public class Exam0431 {
     File file = new File("temp/b/test.txt");
 
     // 파일을 생성하기 전에 존재하지 않는 폴더를 생성하고 싶다면,
-    // File dir = new File(file.getParent());
+    //    String path = file.getParent(); // => temp/b
+    //    File dir = new File(path);
     File dir = file.getParentFile(); // 위의 코드와 같다.
     System.out.println(dir.getCanonicalPath());
 
