59200bdc798b42c720d58fe5f850e5b62b5a3918
Jinyoung Eom <jinyoung.eom@gmail.com>
2020-09-21 20:31:21 +0900

    .

--- src/main/java/com/eomcs/io/ex08/Exam0210.java
+++ src/main/java/com/eomcs/io/ex08/Exam0210.java
@@ -10,8 +10,8 @@ public class Exam0210 {
     FileInputStream in = new FileInputStream("temp/jls11.pdf");
 
     // 기존의 FileInputStream에 버퍼 기능을 덧붙이기 위해서 상속 대신 포함하는 방식을 사용한다.
-    // 다음과 BufferedInputStream 은 FileInputStream을 포함한다.
-    // 즉 파일에서 데이터를 읽을 때 FileInputStream을 사용하는 것이다.
+    // 다음 BufferedInputStream 은 FileInputStream을 포함한다.
+    // 즉 파일에서 데이터를 읽을 때 FileInputStream을 사용한다.
     BufferedInputStream in2 = new BufferedInputStream(in);
 
     int b;
