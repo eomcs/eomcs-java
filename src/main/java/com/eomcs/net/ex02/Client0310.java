58ee563f87687459f74255602aec38c46a862ea6
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-28 13:16:38 +0900

    .

--- src/main/java/com/eomcs/net/ex02/Client0310.java
+++ src/main/java/com/eomcs/net/ex02/Client0310.java
@@ -21,9 +21,12 @@ public class Client0310 {
 
     // 3) 서버와의 연결을 시도한다.
     // => 타임아웃으로 지정된 시간 안에 서버와 연결되지 않으면 즉시 예외가 발생한다.
+    // => Windows의 경우, 
+    //    - 로컬에 접속할 때 타임아웃 설정이 정상적으로 동작되지 않는다.(확인 할 것!)
+    //    - 원격 윈도우 PC에 서버를 실행하여 접속한다면 정상적으로 동작한다.
     //
     System.out.println("서버와 연결 중...");
-    socket.connect(socketAddress, 5000); // timeout : milliseconds
+    socket.connect(socketAddress, 10000); // timeout : milliseconds
     System.out.println("서버와 연결되었음!");
 
     keyScan.nextLine(); // 사용자가 엔터를 칠 때까지 다음 코드로 이동하지 않는다.
--- src/main/java/com/eomcs/net/ex02/Server0210.java
+++ src/main/java/com/eomcs/net/ex02/Server0210.java
@@ -19,6 +19,7 @@ public class Server0210 {
     // 클라이언트는 내부에 설정된 시간(timeout)동안 기다리다 응답을 받지 못하면
     // 예외를 던지고 연결 요청을 취소한다.
     // => new ServerSocket(포트번호, 대기열크기);
+    // 다음과 같이 대기열의 개수를 지정하지 않으면, 기본이 50개이다.
     //
     ServerSocket ss = new ServerSocket(8888);
     System.out.println("서버 소켓 생성 완료!");
