914e396442b72f462ff9fb27d37f142ff92f2e49
eomjinyoung <jinyoung.eom@gmail.com>
2020-09-28 17:47:30 +0900

    .

--- src/main/java/com/eomcs/net/ex03/Client0160.java
+++ src/main/java/com/eomcs/net/ex03/Client0160.java
@@ -22,9 +22,10 @@ public class Client0160 {
       keyScan.nextLine();
 
       out.println("ABC가각간");
-      // out.flush();
-      // byte stream에서 버퍼를 사용할 때는
+      out.flush();
+      // 버퍼를 사용할 때는
       // 데이터를 보내기 위해 반드시 flush()를 호출해야 한다.
+      // => 버퍼에 남아 있는 데이터를 연결된 출력 스트림을 이용하여 내보낸다.
       System.out.println("서버에 데이터를 보냈음!");
 
       String str = in.nextLine();
--- src/main/java/com/eomcs/net/ex03/Server0160.java
+++ src/main/java/com/eomcs/net/ex03/Server0160.java
@@ -30,9 +30,10 @@ public class Server0160 {
         keyboard.nextLine();
 
         out.println(str);
-        // out.flush();
-        // byte stream에서 버퍼를 사용할 때는
+        out.flush();
+        // 버퍼를 사용할 때는
         // 데이터를 보내기 위해 반드시 flush()를 호출해야 한다.
+        // => 버퍼에 남아 있는 데이터를 연결된 출력 스트림을 이용하여 내보낸다.
         System.out.println("클라인트에게 데이터를 보냈음.");
 
       }
