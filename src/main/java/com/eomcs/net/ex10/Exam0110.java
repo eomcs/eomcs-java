6505bcf83f146d7d7247307208bad1f88dbb91a0
eomjinyoung <jinyoung.eom@gmail.com>
2020-10-05 16:57:40 +0900

    .

--- src/main/java/com/eomcs/net/ex10/Exam0110.java
+++ src/main/java/com/eomcs/net/ex10/Exam0110.java
@@ -1,4 +1,4 @@
-// Base64 인코딩
+// Base64 인코딩 : byte[] 인코딩 및 디코딩
 package com.eomcs.net.ex10;
 
 import java.util.Base64;
@@ -9,13 +9,13 @@ public class Exam0110 {
 
   public static void main(String[] args) throws Exception {
     String str = "ABC012가각간";
-    
+
     byte[] bytes = str.getBytes("UTF-8");
     for (byte b : bytes) {
       System.out.printf("%x ", b);
     }
     System.out.println();
-    
+
     // Base64 인코딩
     // => 바이너리 데이터를 문자화시킨다.
     // => 바이너리 값을 6비트식 잘라서(2의 6승) 
@@ -38,18 +38,18 @@ public class Exam0110 {
       System.out.printf("%x ", b);
     }
     System.out.println();
-    
+
     System.out.println(new String(base64Bytes));
-    
+
     // Base64 디코딩
     // => Base64 코드를 원래의 바이너리 값으로 변환한다.
     //
     Decoder decoder = Base64.getDecoder();
     byte[] bytes2 = decoder.decode(base64Bytes);
-    
+
     System.out.println(new String(bytes2, "UTF-8"));
-    
-    
+
+
   }
 
 }
---
+++ src/main/java/com/eomcs/net/ex10/Exam0120.java
@@ -0,0 +1,23 @@
+// Base64 인코딩 : 이미지 파일 --> 인코딩
+package com.eomcs.net.ex10;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class Exam0120 {
+
+  public static void main(String[] args) throws Exception {
+    File file = new File("./sample/test1.jpg");
+
+    FileInputStream in = new FileInputStream(file);
+    byte[] bytes = in.readAllBytes();
+    in.close();
+
+    Encoder encoder = Base64.getEncoder();
+    byte[] encodedBytes = encoder.encode(bytes);
+    System.out.println(new String(encodedBytes));
+  }
+
+}
