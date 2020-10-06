b156eb8e7989f053f4a01c5f441e0e72311d68c1
eomjinyoung <jinyoung.eom@gmail.com>
2020-10-06 14:27:15 +0900

    .

---
+++ src/main/java/com/eomcs/net/ex11/step09/CalculatorServer.java
@@ -0,0 +1,21 @@
+// 계산기 서버 만들기 - 9단계: 리팩토링
+// - 클라이언트의 요청을 처리하는 메서드를 별도의 클래스로 분리하기
+package com.eomcs.net.ex11.step09;
+
+import java.net.ServerSocket;
+
+public class CalculatorServer {
+  public static void main(String[] args) {
+
+    try (ServerSocket serverSocket = new ServerSocket(8888)) {
+      System.out.println("서버 실행 중...");
+
+      new RequestProcessor(serverSocket.accept()).service();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+
+}
