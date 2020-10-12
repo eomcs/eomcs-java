13ef200b6fc51bf694e562a979506ed8218fb4fc
eomjinyoung <jinyoung.eom@gmail.com>
2020-10-12 18:05:54 +0900

    v33

--- src/main/java/com/eomcs/design_pattern/observer/after/h/AbstractCarObserver.java
+++ src/main/java/com/eomcs/design_pattern/observer/after/h/AbstractCarObserver.java
@@ -7,13 +7,24 @@ package com.eomcs.design_pattern.observer.after.h;
 // 즉 이 클래스 자체를 사용하려는 것이 아니다.
 // 이런 경우 추상 클래스로 정의하면 좋다.
 // - 추상 메서드가 없지만, 추상 클래스로 선언함으로써
-// 개발자에게 이 클래스의 역할을 알리는 효과가 있다.
+//   개발자에게 이 클래스의 역할을 알리는 효과가 있다.
+//
+// 인터페이스를 구현한 추상 클래스는 
+// 보통 그 클래스 이름을 'Abstract-'로 시작한다.
 //
 public abstract class AbstractCarObserver implements CarObserver {
 
   @Override
-  public void carStarted() {}
+  public void carStarted() {
+    // 서브 클래스에게 구현된 메서드를 상속해주기 위해
+    // 수퍼 클래스에서 미리 구현한다.
+    // 단, 아무런 코드를 넣지 않는다.
+  }
 
   @Override
-  public void carStopped() {}
+  public void carStopped() {
+    // 서브 클래스에게 구현된 메서드를 상속해주기 위해
+    // 수퍼 클래스에서 미리 구현한다.
+    // 단, 아무런 코드를 넣지 않는다.
+  }
 }
