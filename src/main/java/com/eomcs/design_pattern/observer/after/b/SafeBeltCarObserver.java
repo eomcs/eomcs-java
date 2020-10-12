16af365f9f3298be0f5bf060359858b63798357b
eomjinyoung <jinyoung.eom@gmail.com>
2020-10-12 12:50:52 +0900

    소스정리

--- src/main/java/com/eomcs/design_pattern/observer/after/b/SafeBeltCarObserver.java
+++ src/main/java/com/eomcs/design_pattern/observer/after/b/SafeBeltCarObserver.java
@@ -4,10 +4,15 @@ public class SafeBeltCarObserver implements CarObserver {
 
   @Override
   public void carStarted() {
+    // 자동차의 시동을 걸 때 호출되는 메서드이다.
+    // - 시동 걸 때 뭔가 작업하고 싶다면 이 메서드에 그 코드를 작성하면 된다.
     System.out.println("안전벨트 착용 여부 검사");
   }
 
   @Override
-  public void carStopped() {}
+  public void carStopped() {
+    // 자동차의 시동을 끌 때 호출되는 메서드이다.
+    // - 시동 끌 때 뭔가 작업하고 싶다면 이 메서드에 그 코드를 작성하면 된다.
+  }
 
 }
--- src/main/java/com/eomcs/design_pattern/observer/after/b/Test01.java
+++ src/main/java/com/eomcs/design_pattern/observer/after/b/Test01.java
@@ -6,9 +6,14 @@ public class Test01 {
 
     Car car = new Car();
 
+    // 새 기능이 들어 있는 객체를 Car(publisher)에 등록한다.
+    // - Car 클래스를 손대지 않고 새 기능을 추가하는 방법이다.
+    // - 이것이 Observer 패턴으로 구조화시킨 이유이다.
     car.addCarObserver(new SafeBeltCarObserver());
 
     car.start();
+    // Car 객체는 start()가 호출되면 
+    // 등록된 모든 subscriber(observer/listener)에게 통지(메서드 호출)한다.
 
     car.run();
 
@@ -17,8 +22,7 @@ public class Test01 {
     // 프로젝트 완료한 다음 시간이 지난 후
     // 1) 자동차의 시동을 걸 때 안전벨트 착용 여부를 검사하는 기능을 추가한다.
     // - 자동차의 시동을 걸릴 때 보고를 받을 객체(SafeBeltCarObserver)를 준비한다.
-    // - 시동 걸 때 수행할 기능을 정의한다.
-    // carStarted() 메서드 정의
+    // - 시동 걸 때 수행할 기능을 정의한다. 즉 carStarted() 메서드 정의
     // - Car 객체에 관찰자를 등록한다.
   }
 
