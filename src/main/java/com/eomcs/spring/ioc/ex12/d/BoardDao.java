b2d8e80dbc27cabce635637b9ddeaae0e7a0337a
eomjinyoung <jinyoung.eom@gmail.com>
2020-12-16 12:31:03 +0900

    .

--- src/main/java/com/eomcs/spring/ioc/ex12/d/BoardDao.java
+++ src/main/java/com/eomcs/spring/ioc/ex12/d/BoardDao.java
@@ -6,16 +6,16 @@ import com.eomcs.spring.ioc.ex12.Board;
 
 // 이 인터페이스의 구현체를 mybatis가 자동으로 생성하여
 // Spring IoC 컨테이너에 등록할 것이다.
-// 그럴려면 인터페이스를 정의할 때 몇가지 규칙을 따라야 한다.
+// 그러려면 인터페이스를 정의할 때 몇가지 규칙을 따라야 한다.
 // 1) 맵퍼 파일의 namespace와 인터페이스명(패키지포함)과 같아야 한다.
-// 예) <mapper namespace="com.eomcs.spring.ioc.ex12.d.BoardDao">
+//      예) <mapper namespace="com.eomcs.spring.ioc.ex12.d.BoardDao">
 // 2) 메서드의 이름과 SQL 아이디가 같아야 한다.
 // 3) 메서드의 리턴 타입과 SQL 맵퍼 파일의
-// resultType 또는 resultMap이 같아야 한다.
-// insert, update, delete일 경우
-// 리턴 값이 void 또는 int가 될 수 있다.
+//    resultType 또는 resultMap이 같아야 한다.
+//    insert, update, delete일 경우
+//    리턴 값이 void 또는 int가 될 수 있다.
 // 4) SQL 맵퍼는 파라미터 값을 한 개만 받기 때문에
-// DAO 인터페이스의 메서드도 한 개의 파라미터를 가져야 한다.
+//    DAO 인터페이스의 메서드도 최대 한 개의 파라미터를 가져야 한다.
 //
 public interface BoardDao {
   List<Board> selectList(Map<String, Object> map);
--- src/main/java/com/eomcs/spring/ioc/ex12/d/Exam01.java
+++ src/main/java/com/eomcs/spring/ioc/ex12/d/Exam01.java
@@ -1,9 +1,12 @@
 // Spring과 Mybatis 연동 : mybatis가 구현한 DAO 객체 사용하기
 package com.eomcs.spring.ioc.ex12.d;
 
+import java.util.HashMap;
+import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.eomcs.spring.ioc.SpringUtils;
+import com.eomcs.spring.ioc.ex12.Board;
 
 public class Exam01 {
 
@@ -13,28 +16,29 @@ public class Exam01 {
 
     SpringUtils.printBeanList(iocContainer);
 
-    // BoardDao boardDao = iocContainer.getBean(BoardDao.class);
+    BoardDao boardDao = iocContainer.getBean(BoardDao.class);
+
     //
     // //1) 게시물 입력
-    // Board board = new Board();
-    // board.setTitle("제목입니다.");
-    // board.setContent("내용입니다.");
-    // boardDao.insert(board);
+    Board board = new Board();
+    board.setTitle("제목입니다.");
+    board.setContent("내용입니다.");
+    boardDao.insert(board);
     //
     // //2) 게시물 목록 조회
     // // => selectList()의 파라미터 값을 한 개만 넘겨야 하기 때문에
     // // 여러 개의 값을 넣고 싶으면 Map에 담아 넘긴다.
-    // HashMap<String,Object> params = new HashMap<>();
-    // params.put("startIndex", 0);
-    // params.put("pageSize", 5);
+    HashMap<String,Object> params = new HashMap<>();
+    params.put("startIndex", 0);
+    params.put("pageSize", 5);
     //
-    // List<Board> list = boardDao.selectList(params);
-    // for (Board b : list) {
-    // System.out.printf("%d, %s, %s\n",
-    // b.getNo(),
-    // b.getTitle(),
-    // b.getRegisteredDate());
-    // }
+    List<Board> list = boardDao.selectList(params);
+    for (Board b : list) {
+      System.out.printf("%d, %s, %s\n",
+          b.getNo(),
+          b.getTitle(),
+          b.getRegisteredDate());
+    }
   }
 
 }
