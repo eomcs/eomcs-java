ae94f2acf54c388620c8b0f036fdbabd8ea192d9
Jinyoung Eom <jinyoung.eom@gmail.com>
2021-01-02 01:08:30 +0900

    소스정리

--- src/main/java/com/eomcs/oop/ex06/c/Exam0010.java
+++ src/main/java/com/eomcs/oop/ex06/c/Exam0010.java
@@ -1,21 +1,21 @@
-// 다형성 - 오버라이딩을 하는 이유? 
-package ch14.a;
+// 오버라이딩(overriding) - 오버라이딩을 하는 이유?
+package com.eomcs.oop.ex06.c;
 
-public class Test01 {
-
+public class Exam0010 {
   public static void main(String[] args) {
+
     Score2 score = new Score2();
-    
+
     score.kor = 100;
     score.eng = 100;
     score.math = 100;
     score.music = 100;
     score.art = 100;
-    
+
+    // 새로 추가한 필드에 맞춰 계산이 달라야 한다.
     score.compute();
-    
+
     System.out.printf("%d(%f)\n", score.getSum(), score.getAver());
 
   }
-
 }
