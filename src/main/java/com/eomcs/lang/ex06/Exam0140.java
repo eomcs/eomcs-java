52a6e3215b28537c9b9ce9e0ab096ff40cff0615
eomjinyoung <jinyoung.eom@gmail.com>
2021-01-15 17:32:07 +0900

    소스정리

--- src/main/java/com/eomcs/lang/ex06/Exam0140.java
+++ src/main/java/com/eomcs/lang/ex06/Exam0140.java
@@ -41,25 +41,5 @@ public class Exam0140 {
       System.out.println("미성년입니다.");
       System.out.println("--------------------------");
     }
-
-    // 잘못 사용한 예:
-    // => else 문만 단독으로 사용할 수 없다.
-    //    항상 if 문과 짝을 이뤄 사용한다.
-    /*
-        else
-            System.out.println("컴파일 오류!");
-     */
-
-    // => if 문과 else 문 사이에 다른 문장이 올 수 없다.
-    /*
-        if (age >= 19)
-            System.out.println("성인입니다.");
-        System.out.println("--------------------------");
-        else
-            System.out.println("미성년입니다.");
-     */
-
-
-
   }
 }
--- src/main/java/com/eomcs/lang/ex06/Exam0321.java
+++ src/main/java/com/eomcs/lang/ex06/Exam0321.java
@@ -14,7 +14,7 @@ public class Exam0321 {
     sum = 0;
     while (count < 100) {
       count++;
-      if (count % 2 == 0) {
+      if ((count & 0x01) == 0) { // count & 0x01 ==> count % 2
         sum += count;
       }
     }
