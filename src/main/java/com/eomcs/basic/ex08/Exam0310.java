9a0bcd5e3b7f496e2c8c16b1c00110bbb3a4d70b
eomjinyoung <jinyoung.eom@gmail.com>
2021-02-17 17:36:13 +0900

    .

--- src/main/java/com/eomcs/basic/ex08/Exam0310.java
+++ src/main/java/com/eomcs/basic/ex08/Exam0310.java
@@ -15,15 +15,19 @@ public class Exam0310 {
     map.put("s05", new Member("윤봉길", 30));
 
     Set<String> keys = map.keySet();
+    // keySet() 이 리턴하는 것은 HashMap의 key 목록을 다루는 객체이다.
+    // 별도로 key 목록을 만들어 리턴하지 않는다.
 
-    // 키목록을 얻은 후에 값을 삭제한다.
+    // 따라서 다음과 같이 맵의 데이터를 지운다면,
+    // 당연히 키 목록에서 해당 키도 삭제될 것이다.
     map.remove("s01");
     map.remove("s02");
     map.remove("s03");
 
-    // Set 객체를 통해 key 를 꺼낼 때,
-    // 그 순간의 HashSet에 있는 key를 꺼낸다.
-    // 즉 keySet()을 호출할 때 모든 key를 미리 목록을 만들어 리턴하지 않는다.
+    // 그런 후 Set 객체를 통해 key 를 꺼낼 때,
+    // Map에서 제거된 키는 꺼낼 수 없다.
+    // 왜?
+    // - Set 객체는 HashMap에 있는 key를 꺼내기 때문이다.
     for (Object key : keys) {
       System.out.println(key);
     }
