1232ae126bdebdf93a89a482892c0d1427634b0b
eomjinyoung <jinyoung.eom@gmail.com>
2021-03-16 14:04:12 +0900

    .

--- src/main/java/com/eomcs/concurrent/ex6/Exam0140.java
+++ src/main/java/com/eomcs/concurrent/ex6/Exam0140.java
@@ -14,7 +14,9 @@ public class Exam0140 {
         this.count = count;
 
         // 이 객체의 사용을 기다리는 스레드에게 작업을 시작할 것을 알린다.
+        //synchronized (this) {
         this.notify();
+        //}
         // 문법 주의!
         // => notify()도 동기화 영역에서 호출해야 한다.
         // => 안그러면 IllegalMonitorStateException 예외가 발생한다.
@@ -31,17 +33,26 @@ public class Exam0140 {
       @Override
       public void run() {
 
-        // 어떤 객체를 대상으로 여러 스레드가 동시에 사용하지 못하게 할 것인지
-        // 지정해야 한다.
-        // 동기화 블록 문법:
-        // => synchronized(접근대상) {...}
-        //
-        synchronized (valueBox) {
-          System.out.println("스레드 시작했음!");
-          try {
-            while (true) {
-              System.out.println("스레드 대기중...");
+        System.out.println("스레드 시작했음!");
+        try {
+          while (true) {
+            System.out.println("스레드 대기중...");
 
+            // wait()
+            // - 해당 객체에서 notify()를 통해 알림이 올 때까지 스레드의 실행을 멈추게 한다.
+            // - 이 메서드는 동기화 블록
+            //   (한 번에 한 스레드만이 진입하도록 설정된 블록)에서만 호출할 수 있다.
+            // 
+            // 문법 주의!
+            // => wait()/notify() 는 반드시 동기화 영역 안에서 호출해야 한다.
+            // 
+            // 동기화 영역?
+            // => synchronized로 선언된 메서드
+            //    예) synchronized void m() {}
+            // => synchronized로 묶인 블록
+            //    예) synchronized(접근대상) {...}
+            //
+            synchronized (valueBox) {
               valueBox.wait();
               // valueBox 객체에 대해 사용하라는 신호가 올 때까지 이 스레드에게 기다리라는 명령이다.
               // 즉 wait()를 호출한 스레드는 Not Runnable 상태에 진입한다.
@@ -49,25 +60,20 @@ public class Exam0140 {
               // 
               // 이 스레드를 잠에서 깨우는 방법?
               // => 다른 스레드가 valueBox에 대해 notify()를 호출하면,
-              //    wait() 로 신호를 기다리고 있는 스레드가 잠에서 깨어나 실행을 시작한다.
-              //
-              // 문법 주의!
-              // => wait()/notify() 는 반드시 동기화 영역 안에서 호출해야 한다.
-              // 동기화 영역?
-              // => synchronized로 선언된 메서드
-              // => synchronized로 묶인 블록
+              //    wait() 로 신호를 기다리고 있는 스레드가 잠에서 깨어나 
+              //    실행을 시작한다.
               //
               // 기다림을 끝내는 방법?
-              // => notify()를 통해 기다림이 끝났다는 것을 알림 받아야 한다.
+              // => 즉 notify()를 통해 기다림이 끝났다는 것을 알림 받아야 한다.
               //
-              System.out.println("카운트 시작!");
-              for (int i = valueBox.count; i > 0; i--) {
-                System.out.println("==> " + i);
-              }
             }
-          } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("카운트 시작!");
+            for (int i = valueBox.count; i > 0; i--) {
+              System.out.println("==> " + i);
+            }
           }
+        } catch (Exception e) {
+          e.printStackTrace();
         }
       }
     }
