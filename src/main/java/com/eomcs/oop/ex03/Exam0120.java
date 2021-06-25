cccb60df4db7d6bb945833e24ac6346dc00a491d
eomjinyoung <jinyoung.eom@gmail.com>
2021-06-25 18:49:56 +0900

    .

--- src/main/java/com/eomcs/oop/ex03/Exam0120.java
+++ src/main/java/com/eomcs/oop/ex03/Exam0120.java
@@ -3,20 +3,21 @@
 package com.eomcs.oop.ex03;
 
 public class Exam0120 {
+
+  // 1) 성적 데이터를 설계할 클래스이기 때문에 그에 맞는 클래스명을 사용하라!
+  static class Score {
+
+    // 2) 여러 명의 구별되는 성적 데이터를 저장해야 하기 때문에
+    // 인스턴스 변수로 메모리를 설계하라!
+    String name;
+    int kor;
+    int eng;
+    int math;
+    int sum;
+    float average;
+  }
+
   public static void main(String[] args) {
-
-    // 1) 성적 데이터를 설계할 클래스이기 때문에 그에 맞는 클래스명을 사용하라!
-    class Score {
-
-      // 2) 여러 명의 구별되는 성적 데이터를 저장해야 하기 때문에
-      // 인스턴스 변수로 메모리를 설계하라!
-      String name;
-      int kor;
-      int eng;
-      int math;
-      int sum;
-      float average;
-    }
 
     // 저장하고 싶은 데이터 개수 만큼 인스턴스를 생성하라!
     Score s1 = new Score(); // 1명 분의 성적 데이터를 저장할 메모리
