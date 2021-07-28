b5ff1d9b04e80ca770219d01810ea2703cd4fefe
eomjinyoung <jinyoung.eom@gmail.com>
2021-07-28 17:39:57 +0900

    .

--- src/main/java/com/eomcs/oop/ex05/e/A.java
+++ src/main/java/com/eomcs/oop/ex05/e/A.java
@@ -1,6 +1,6 @@
 package com.eomcs.oop.ex05.e;
 
-public class A {
+public class A { 
   void m1() {
     System.out.println("A.m1()");
   }
--- src/main/java/com/eomcs/oop/ex05/e/Exam01.java
+++ src/main/java/com/eomcs/oop/ex05/e/Exam01.java
@@ -7,9 +7,9 @@ public class Exam01 {
     B obj = new B();
 
     // B 인스턴스를 이용하여 B가 사용권을 획득한 A 클래스의 메서드를 호출할 수 있다.
-    obj.m1();
+    obj.m1(); // A 클래스의 m1() 호출
 
-    obj.m2();
+    obj.m2(); // B 클래스의 m2() 호출
 
     // 실험:
     // bin/main/.../A.class 파일을 제거한 후 다시 실행하라!
