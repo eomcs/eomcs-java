f01e2194d8fe99f5d5f3308af00aa4ec129a8a52
eomjinyoung <jinyoung.eom@gmail.com>
2021-07-30 18:01:25 +0900

    .

--- src/main/java/com/eomcs/oop/ex08/b/Exam0210.java
+++ src/main/java/com/eomcs/oop/ex08/b/Exam0210.java
@@ -13,7 +13,7 @@ public class Exam0210 extends com.eomcs.oop.ex08.b.sub.C {
   public static void main(String[] args) {
     A obj1 = new A();
 
-    //obj1.privateVar = 100; // 접근 불가! 오직 그 클래스 안에서만 사용가능.
+    //    obj1.privateVar = 100; // 접근 불가! 오직 그 클래스 안에서만 사용가능.
     obj1.defaultVar = 100; // OK! 이 클래스는 A 클래스와 같은 패키지에 소속되어 있다.
     obj1.protectedVar = 100; // OK! 비록 이 클래스가 자식클래스는 아니지만
     // 같은 패키지에 소속되어 있다.
@@ -21,24 +21,24 @@ public class Exam0210 extends com.eomcs.oop.ex08.b.sub.C {
 
     com.eomcs.oop.ex08.b.sub.B obj2 = new com.eomcs.oop.ex08.b.sub.B();
 
-    //obj2.privateVar = 100; // 접근 불가! 오직 그 클래스 안에서만 사용 가능.
-    //obj2.defaultVar = 100; // 접근 불가! 같은 패키지까지만 접근 가능.
-    //obj2.protectedVar = 100; // 접근 불가! 같은 패키지 또는 자식 클래스 접근 가능
+    //    obj2.privateVar = 100; // 접근 불가! 오직 그 클래스 안에서만 사용 가능.
+    //    obj2.defaultVar = 100; // 접근 불가! 같은 패키지까지만 접근 가능.
+    //    obj2.protectedVar = 100; // 접근 불가! 같은 패키지 또는 자식 클래스 접근 가능
     obj2.publicVar = 100; // OK! 모두 다 접근 가능.
 
 
     com.eomcs.oop.ex08.b.sub.C obj3 = new com.eomcs.oop.ex08.b.sub.C();
 
-    //obj3.privateVar = 100; // 접근 불가! 오직 그 클래스 안에서만 사용 가능.
-    //obj3.defaultVar = 100; // 접근 불가! 같은 패키지까지만 접근 가능.
-    //obj3.protectedVar = 100; // 접근 불가! 같은 패키지 또는 자식 클래스 접근 가능
+    //    obj3.privateVar = 100; // 접근 불가! 오직 그 클래스 안에서만 사용 가능.
+    //    obj3.defaultVar = 100; // 접근 불가! 같은 패키지까지만 접근 가능.
+    //    obj3.protectedVar = 100; // 접근 불가! 같은 패키지 또는 자식 클래스 접근 가능
     // 자식 클래스인데 접근 불가?
     // 이유 => 자기의 인스턴스 변수가 아니다.
     obj3.publicVar = 100; // OK! 모두 다 접근 가능.
 
     Exam0210 obj4 = new Exam0210();
-    //obj4.privateVar = 100; // 접근 불가! C 클래스에서만 접근 가능
-    //obj4.defaultVar = 100; // 접근 불가! C 클래스와 같은 패키지가 아니다.
+    //    obj4.privateVar = 100; // 접근 불가! C 클래스에서만 접근 가능
+    //    obj4.defaultVar = 100; // 접근 불가! C 클래스와 같은 패키지가 아니다.
     obj4.protectedVar = 100; // OK! Exam0210은 C의 자식 클래스이며,
     // 또한 C로부터 상속 받아서 만든 자기 변수이다.
     obj4.publicVar = 100;
