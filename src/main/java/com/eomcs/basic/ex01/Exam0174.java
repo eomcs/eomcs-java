8325f384c79f739a2cb4eb8535b29dde40f5bda4
eomjinyoung <jinyoung.eom@gmail.com>
2021-08-10 14:18:45 +0900

    .

--- src/main/java/com/eomcs/basic/ex01/Exam0174.java
+++ src/main/java/com/eomcs/basic/ex01/Exam0174.java
@@ -1,17 +1,17 @@
 // Object 클래스 - clone() : deep copy
 package com.eomcs.basic.ex01;
- 
+
 public class Exam0174 {
-  
+
   static class Engine implements Cloneable {
     int cc;
     int valve;
-    
+
     public Engine(int cc, int valve) {
       this.cc = cc;
       this.valve = valve;
     }
-    
+
     @Override
     public String toString() {
       return "Engine [cc=" + cc + ", valve=" + valve + "]";
@@ -22,12 +22,12 @@ public class Exam0174 {
       return (Engine) super.clone();
     }
   }
-  
+
   static class Car implements Cloneable {
     String maker;
     String name;
     Engine engine;
-    
+
     public Car(String maker, String name, Engine engine) {
       this.maker = maker;
       this.name = name;
@@ -50,23 +50,24 @@ public class Exam0174 {
       return copy;
     }
   }
-  
+
   public static void main(String[] args) throws Exception {
     Engine engine = new Engine(3000, 16);
     Car car = new Car("비트자동차", "비트비트", engine);
-    
-    // 자동차 복제
+
+    // 자동차 복제 
+    // => Car의 clone()에서 Engine 객체도 복제할 것이다.
     Car car2 = car.clone();
-    
+
     System.out.println(car == car2);
     System.out.println(car);
     System.out.println(car2);
     System.out.println(car.engine == car2.engine);
-    
+
     // car의 엔진과 car2의 엔진이 다른 엔진인지 확인해보자!
     car.engine.cc = 2000;
     System.out.println(car2.engine.cc);
-    
+
   }
 }
 
