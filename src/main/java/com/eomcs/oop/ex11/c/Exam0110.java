a7decd8985936a525f45ea8f658516bef43215a4
eomjinyoung <jinyoung.eom@gmail.com>
2021-08-13 14:42:26 +0900

    .

--- src/main/java/com/eomcs/oop/ex11/c/Exam0110.java
+++ src/main/java/com/eomcs/oop/ex11/c/Exam0110.java
@@ -1,10 +1,21 @@
 // inner class : 클래스 정의와 인스턴스 생성
 package com.eomcs.oop.ex11.c;
 
-class A {
-  class X {
+class X {} // Top Level Class
 
+class A {
+
+  class X { // inner class
+    // 컴파일러는 inner 클래스를 컴파일 할 때 다음과 같이 
+    // - 바깥 클래스의 인스턴스 주소를 저장할 필드를 추가하고,
+    // - 바깥 인스턴스의 주소를 받는 파라미터로 받는 생성자를 만든다.
+    //
+    //    A outer;
+    //    public X(A obj) {
+    //      this.outer = obj;
+    //    }
   }
+
 }
 
 public class Exam0110 {
@@ -21,6 +32,10 @@ public class Exam0110 {
 
     //2) 바깥 클래스의 인스턴스 주소를 사용하여 inner class의 인스턴스 생성
     obj = outer.new X();
+
+    // 컴파일러는 inner 클래스의 객체를 만드는 코드를 컴파일 할 때 다음과 같이 
+    // - 바깥 클래스의 객체를 생성자에 전달하는 코드로 변경한다.
+    //    obj = new X(outer);
 
   }
 
--- src/main/java/com/eomcs/oop/ex11/c/Exam0111.java
+++ src/main/java/com/eomcs/oop/ex11/c/Exam0111.java
@@ -4,7 +4,11 @@ package com.eomcs.oop.ex11.c;
 class A2 {
   class X {
     // inner class 는 스태틱 멤버를 가질 수 없다.
-    // 스태틱 멤버는 오직 top level class 나 static nested class 만이 가질 수 있다.
+    // 스태틱 멤버는 오직 
+    // - top level class 나 
+    // - static nested class 
+    // 만이 가질 수 있다.
+    // 
     //    static int v1; // 컴파일 오류!
     //    static void m1() {} // 컴파일 오류!
     //    static {} // 컴파일 오류!
@@ -21,9 +25,9 @@ public class Exam0111 {
     // 바깥 클래스의 인스턴스를 먼저 만든 다음에 
     // inner 클래스의 인스턴스를 만든다.
     A2 outer = new A2();
-    A2.X obj = outer.new X();
+    A2.X obj = outer.new X(); // => new X(outer)
 
     // 물론 다음과 같이 위의 두 줄을 한 줄로 표현할 수 있다.
-    A2.X obj2 = new A2().new X();
+    A2.X obj2 = new A2().new X(); // => new X(new A2())
   }
 }
