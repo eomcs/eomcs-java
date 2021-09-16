e02c4dfdae16e0bc5907d6e119ed0d4f2097c174
eomjinyoung <jinyoung.eom@gmail.com>
2021-09-16 10:49:50 +0900

    .

--- src/main/java/com/eomcs/basic/ex02/Exam0172.java
+++ src/main/java/com/eomcs/basic/ex02/Exam0172.java
@@ -1,27 +1,19 @@
-// String - 기타 메서드 : join()
+// String - 기타 메서드 : Arrays.copyOfRange()
 package com.eomcs.basic.ex02;
+
+import java.util.Arrays;
 
 public class Exam0172 {
   public static void main(String[] args) {
 
     String[] arr = {"101", "제목", "내용", "4", "2021-2-2"};
 
-    // 1) 배열의 모든 값을 CSV 형식의 한 문자열로 만들기
-    // => format() 사용
-    String s1 = String.format("%s,%s,%s,%s,%s", arr[0], arr[1], arr[2], arr[3], arr[4]);
-    System.out.println(s1);
+    // 배열에서 특정 범위의 항목을 복사하기
+    String[] arr2 = Arrays.copyOfRange(arr, 2, 4);
+    for (String s : arr2) {
+      System.out.println(s);
+    }
 
-    // => 가변 파라미터 자리에 배열을 전달해도 된다.
-    String s2 = String.format("%s,%s,%s,%s,%s", arr);
-    System.out.println(s2);
-
-    // => join() 사용
-    String s3 = String.join(",", arr[0], arr[1], arr[2], arr[3], arr[4]);
-    System.out.println(s3);
-
-    // => 가변 파라미터 자리에 배열을 전달해도 된다.
-    String s4 = String.join(",", arr);
-    System.out.println(s4);
   }
 }
 
