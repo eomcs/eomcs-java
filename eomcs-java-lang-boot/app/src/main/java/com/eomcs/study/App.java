f2314361b28e8ab666b0048e4d7db69aa81c700e
eomjinyoung <jinyoung.eom@gmail.com>
2021-12-01 09:52:01 +0900

    .

---
+++ eomcs-java-lang-boot/app/src/main/java/com/eomcs/study/App.java
@@ -0,0 +1,21 @@
+package com.eomcs.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+  @RequestMapping("/hello")
+  String hello() {
+    return "Hello World!";
+  }
+
+}
