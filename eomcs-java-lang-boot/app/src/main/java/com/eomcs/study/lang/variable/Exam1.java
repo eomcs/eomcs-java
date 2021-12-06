11ce3495f0d7ea76a139a09e93789a098f8f740a
eomjinyoung <jinyoung.eom@gmail.com>
2021-12-06 11:26:50 +0900

    .

--- eomcs-java-lang-boot/app/src/main/java/com/eomcs/study/lang/variable/Exam1.java
+++ eomcs-java-lang-boot/app/src/main/java/com/eomcs/study/lang/variable/Exam1.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("lang.variable.exam1") // 클래스 이름이 같은 경우 충돌 방지
+@RestController("lang.variable.Exam1") // 클래스 이름이 같은 경우 충돌 방지
 @RequestMapping("/lang/variable/exam1")
 public class Exam1 {
 
