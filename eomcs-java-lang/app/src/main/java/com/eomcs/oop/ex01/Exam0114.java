308d19766309532fee1ba7d0626ba16105200259
eomjinyoung <jinyoung.eom@gmail.com>
2021-12-30 14:23:50 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex01/Exam0114.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex01/Exam0114.java
@@ -34,10 +34,10 @@ public class Exam0114 {
   static Score createScore(String name, int kor, int eng, int math) {
     Score s = new Score();
 
-    s.name = "홍길동";
-    s.kor = 100;
-    s.eng = 90;
-    s.math = 80;
+    s.name = name;
+    s.kor = kor;
+    s.eng = eng;
+    s.math = math;
 
     return s; // s에 저장된 인스턴스의 주소를 리턴한다.
     // 강사님! 로컬 변수는 메서드 호출이 끝난 다음에 삭제된다고 했는데
