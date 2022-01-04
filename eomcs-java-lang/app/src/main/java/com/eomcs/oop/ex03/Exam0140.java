21b253a8aa74e2d2bb7818e533b6e08baf4b707a
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-04 11:55:19 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0140.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0140.java
@@ -3,6 +3,7 @@
 package com.eomcs.oop.ex03;
 
 public class Exam0140 {
+
   static class A {
     static int v1;
     int v2;
@@ -24,11 +25,11 @@ public class Exam0140 {
     A p = new A();
     // 이제 v2 변수는 Heap에 생성되었다.
     // A클래스의 인스턴스를 만들 때
-    // static 이 안붙은 변수가 그 대상이다.
+    // static 이 안붙은 변수(non-static 변수 = 인스턴스 변수)가 그 대상이다.
     //
     // v2 인스턴스 변수는 인스턴스 주소를 통해 사용해야 한다.
     // 클래스이름으로 사용할 수 없다.
-    // A.v2 = 200; // 컴파일 오류!
+    //    A.v2 = 200; // 컴파일 오류!
 
     p.v2 = 200; // OK!
 
