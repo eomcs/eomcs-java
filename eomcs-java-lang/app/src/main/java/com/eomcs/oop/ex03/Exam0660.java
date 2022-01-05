290a99a961cb60fb2160aa720236819be1302a37
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-05 12:59:48 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0660.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0660.java
@@ -4,19 +4,31 @@ package com.eomcs.oop.ex03;
 public class Exam0660 {
 
   public static class A {
+    static {
+      int x = 200;
+      System.out.println(x);
+      System.out.println("Static{} 11111");
+    }
     static int a;
-    static void m() {}
+
 
     // 클래스가 로딩될 때 스태틱 초기화 블록은 실행된다.
     // 여러 개의 스태틱 블록이 있을 때, 컴파일러는 한 개의 블록으로 합친다.
     // - 바이트코드(Exam0660$A.class)를 확인해 보라.
     //
-    static {
-      System.out.println("Static{} 11111");
-    }
 
     static {
+      String x = "Hello";
+      System.out.println(x);
       System.out.println("Static{} 22222");
+    }
+
+    static void m() {}
+
+    static {
+      int x = 300;
+      System.out.println(x);
+      System.out.println("Static{} 33333");
     }
   }
 
--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0680.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex03/Exam0680.java
@@ -4,6 +4,9 @@ package com.eomcs.oop.ex03;
 public class Exam0680 {
 
   public static class A {
+    static {
+      b = 400;
+    }
 
     static int a = 100;
 
@@ -12,30 +15,37 @@ public class Exam0680 {
       System.out.println("static {} 실행");
     }
 
-    static {
-      b = 400;
-    }
-
     static int b = 300;
 
-
     // 변수 초기화 문장(variable initializer)을 컴파일 할 때,
-    // - 일단 모든 스태틱 필드를 먼저 만든다.
-    // - 스태틱 변수 초기화 문장을 스태틱 블록으로 옮긴다.
-    //   스태틱 블록 모두 순서에 따라 한 스태틱 블록에 합친다.
-    // - 즉, 스태틱 변수 초기화 문장과 스태틱 블록을 구분하지 않고
-    //   선언된 순서대로 하나의 스태틱 블록에 합친다.
-    // - 위의 문장은 다음 문장으로 바뀐다.
+    // 1) 스태틱 변수 선언에서 변수 초기화 문장을 별도의 스태틱 블록으로 분리한다.
     //
-    // static int a;
-    // static int b;
-    // static {
-    //   a = 100;
-    //   a = 200;
-    //   System.out.println("static {} 실행");
-    //   b = 400;
-    //   b = 300;
-    // }
+    //     static {
+    //       b = 400;
+    //     }
+    //     static int a;
+    //     static {
+    //       a = 100;
+    //     }
+    //     static {
+    //       a = 200;
+    //       System.out.println("static {} 실행");
+    //     }
+    //     static int b;
+    //     static {
+    //       b = 300;
+    //     }
+    // 2) 스태틱 블록을 한 개의 블록으로 합친다.
+    //     static int a;
+    //     static int b;
+    //
+    //     static {
+    //       b = 400;
+    //       a = 100;
+    //       a = 200;
+    //       System.out.println("static {} 실행");
+    //       b = 300;
+    //     }
     // - 바이트 코드(Exam0680$A.class)를 확인해 보라!
   }
 
