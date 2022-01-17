c5f16bec1963f342672e7e37ec249cd3867c959b
eomjinyoung <jinyoung.eom@gmail.com>
2022-01-17 12:24:46 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0322.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/io/ex01/Exam0322.java
@@ -7,7 +7,7 @@ public class Exam0322 {
 
   public static void main(String[] args) throws Exception {
 
-    File dir = new File("temp2/a");
+    File dir = new File("temp2/a/b");
 
     // 디렉토리를 생성할 때 존재하지 않는 중간 디렉토리도 만들고 싶다면,
     // mkdirs()를 호출하라.
@@ -16,9 +16,9 @@ public class Exam0322 {
     // - 지정된 경로에 디렉토리가 존재하지 않으면 그 디렉토리도 만든다.
     //
     if (dir.mkdirs()) {
-      System.out.println("temp2/a 디렉토리를 생성하였습니다.");
+      System.out.println("temp2/a/b 디렉토리를 생성하였습니다.");
     } else {
-      System.out.println("temp2/a 디렉토리를 생성할 수 없습니다.");
+      System.out.println("temp2/a/b 디렉토리를 생성할 수 없습니다.");
     }
 
 
