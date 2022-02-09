3575320348fe54a7d8fdc19293033c1631c1ead0
eomjinyoung <jinyoung.eom@gmail.com>
2022-02-09 15:57:58 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
@@ -6,6 +6,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.Socket;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -16,66 +17,43 @@ import javax.swing.UIManager;
 public class ChatClient extends JFrame {
   private static final long serialVersionUID = 1L;
 
+  Socket socket;
+
+  JTextField addressTf = new JTextField(30);
+  JTextField portTf = new JTextField(4);
+  JTextArea messageListTa = new JTextArea();
+  JTextField messageTf = new JTextField(35);
+
   public ChatClient() {
     super("계산기2");
     addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
+        try {socket.close();} catch (Exception ex) {}
         System.exit(0);
       }
     });
     setSize(500, 400);
 
     Container contentPane = this.getContentPane();
-
     JPanel topPanel = new JPanel();
     topPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자를 교체
-
-    JTextField addressTf = new JTextField(30);
     topPanel.add(addressTf);
-
-    JTextField portTf = new JTextField(4);
     topPanel.add(portTf);
-
     JButton connectBtn = new JButton("연결");
-
-    // 1) 로컬 클래스
-    //    class MyActionListener implements ActionListener {
-    //      @Override
-    //      public void actionPerformed(ActionEvent e) {
-    //      }
-    //    }
-    //    connectBtn.addActionListener(new MyActionListener());
-
-    // 2) 익명 클래스
-    //    connectBtn.addActionListener(new ActionListener() {
-    //      @Override
-    //      public void actionPerformed(ActionEvent e) {
-    //      }
-    //    });
-
-    // 3) 람다(lambda) 문법
-    //    connectBtn.addActionListener(e -> System.out.println("연결 버튼 눌렀음!"));
-
-    // 4) 메서드 레퍼런스
     connectBtn.addActionListener(this::connectChatServer);
-
     topPanel.add(connectBtn);
-
     contentPane.add(topPanel, BorderLayout.NORTH);
 
-    JTextArea messageListTa = new JTextArea();
+
     contentPane.add(messageListTa, BorderLayout.CENTER);
 
     JPanel bottomPanel = new JPanel();
     bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자를 교체
-
-    JTextField messageTf = new JTextField(35);
     bottomPanel.add(messageTf);
-
     JButton sendBtn = new JButton("보내기");
+    sendBtn.addActionListener(this::sendMessage);
     bottomPanel.add(sendBtn);
-
     contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
     setVisible(true);
@@ -95,10 +73,22 @@ public class ChatClient extends JFrame {
 
   public void connectChatServer(ActionEvent e) {
     System.out.println("서버에 연결하기!");
+    System.out.println(addressTf.getText());
+    System.out.println(portTf.getText());
+
+    try {
+      socket = new Socket(
+          addressTf.getText(), 
+          Integer.parseInt(portTf.getText()));
+
+    } catch (Exception ex) {
+      System.out.println("서버에 연결 중 오류 발생!");
+    }
   }
 
   public void sendMessage(ActionEvent e) {
     System.out.println("메시지 보내기");
+    System.out.println(messageTf.getText());
   }
 }
 
