805e46bf87ba0f307ac7997816ab9f2b8a9bfe8b
eomjinyoung <jinyoung.eom@gmail.com>
2022-02-10 19:28:48 +0900

    .

--- eomcs-java-lang/app/src/main/java/Test.java
+++ eomcs-java-lang/app/src/main/java/Test.java
@@ -1,20 +1,17 @@
-import java.util.ArrayList;
-
 public class Test {
 
   public static void main(String[] args) {
-    ArrayList list = new ArrayList();
-    list.add(100);
-    list.add(200);
-    list.add(400);
-    list.add(500);
+    int x = 100;
 
-    for (int i = 0; i < list.size(); i++) {
-      System.out.println(list.get(i));
-      if (i == 2) {
-        list.remove(2);
-      }
-    }
+    test(x++);
+
+    int temp = x;
+    x = x + 1;
+    test(temp);
+  }
+
+  static void test(int a) {
+    System.out.println(a);
   }
 
 }
