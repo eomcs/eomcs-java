f78a79e8a0ea1bf5841031e70ecace5cf05ce0cc
eomjinyoung <jinyoung.eom@gmail.com>
2022-02-14 12:51:24 +0900

    .

---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/algorithm/data_structure/linkedlist2/step1/Node.java
@@ -0,0 +1,19 @@
+package com.eomcs.algorithm.data_structure.linkedlist2.step1;
+
+public class Node {
+  Node prev;
+  Object value;
+  Node next;
+
+  public Node() {}
+
+  public Node(Object value) {
+    this.value = value;
+  }
+
+  public Node(Object value, Node prev, Node next) {
+    this.value = value;
+    this.prev = prev;
+    this.next = next;
+  }
+}
