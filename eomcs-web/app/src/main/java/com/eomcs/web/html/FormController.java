9d26c8dc1070324e8eb120fb9a28134c540af49e
eomjinyoung <jinyoung.eom@gmail.com>
2022-03-23 14:55:08 +0900

    .

--- eomcs-web/app/src/main/java/com/eomcs/web/html/FormController.java
+++ eomcs-web/app/src/main/java/com/eomcs/web/html/FormController.java
@@ -1,11 +1,18 @@
 package com.eomcs.web.html;
 
+import java.io.File;
 import java.util.HashMap;
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FormController {
+
+  @Autowired
+  ServletContext sc; // 서버에 배포된 웹 애플리케이션의 정보를 다루는 객체
 
   @RequestMapping("/html/form/exam01")
   public Object exam01() {
@@ -19,5 +26,22 @@ public class FormController {
     map.put("age", age);
 
     return map; 
+  }
+
+  @RequestMapping("/html/form/exam11")
+  public Object exam11(String name, int age, MultipartFile photo) {
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(photo.getOriginalFilename());
+
+    try {
+      File photoFile = new File("c:/upload/" + photo.getOriginalFilename());
+      photo.transferTo(photoFile);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "error!";
+    }
+
+    return "ok!"; 
   }
 }
