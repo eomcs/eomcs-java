034532da8fd50dc3e67155d37fedb24cd03946c9
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-19 10:20:12 +0900

    .:

---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/sol/Test01.java
@@ -0,0 +1,31 @@
+package com.eomcs.quiz.ex01.sol;
+
+// [문제] 
+// 파라미터로 주어진 정수 값을 2진수로 표현했을 때 1로 설정된 비트의 개수를 구하라!
+// [훈련 목표]
+// - 관계 연산자 및 비트 연산자, 비트 이동 연산자 활용
+// - 반복문 활용
+// - 메서드 파라미터 및 리턴 값 다루기
+//
+public class Test01 {
+
+  public static void main(String[] args) {
+    int c = countBits(0b01100011);
+    System.out.println(c == 4); // true
+
+    c = countBits(0b01010111_01100011);
+    System.out.println(c == 9); // true
+  }
+
+  static int countBits(int value) {
+    int r = 0;
+
+    while (value != 0) {
+      r += (value & 1); // 1
+      value >>>= 1;
+    }
+
+    return r;
+  }
+
+}
---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/sol/Test02.java
@@ -0,0 +1,33 @@
+package com.eomcs.quiz.ex01.sol;
+
+// [문제] 
+// 패리티(parity)를 구하라!
+// - 1의 개수가 홀수 개이면 1
+// - 1의 개수가 짝수 개이면 0
+// [훈련 목표]
+// - 관계 연산자 및 비트 연산자, 비트 이동 연산자 활용
+// - 반복문 활용
+// - 메서드 파라미터 및 리턴 값 다루기
+//
+public class Test02 {
+
+  public static void main(String[] args) {
+    int p = parity(0b01100011);
+    System.out.println(p == 0); // true
+
+    p = parity(0b01010111_01100011);
+    System.out.println(p == 1); // true
+  }
+
+  static int parity(int value) {
+    int r = 0;
+
+    while (value != 0) {
+      r ^= (value & 1);
+      value >>>= 1;
+    }
+
+    return r;
+  }
+
+}
