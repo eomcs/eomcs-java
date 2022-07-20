9add6e71881ed7f8573baae92149960bd40eb0f3
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-20 13:00:04 +0900

    .

---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex02/test/ExamTest2.java
@@ -0,0 +1,54 @@
+package com.eomcs.oop.ex02.test;
+
+//# 관련된 기능(메서드)을 묶어 분류하기
+//1) 분류 전
+//2) 메서드를 클래스로 묶어 분류하기
+//3) 클래스 변수 도입
+//4) 클래스 변수의 한계 확인
+//5) 인스턴스 변수 도입
+//6) 인스턴스 메서드 활용
+//7) 패키지 멤버 클래스로 분리
+//8) 클래스를 역할에 따라 패키지로 분류하기
+//
+public class ExamTest2 {
+
+  public static void main(String[] args) {
+    // 다음 식을 연산자 우선 순위를 고려하지 않고 순서대로 계산하라!
+    // 2 + 3 - 1 * 7 / 3 = ?
+
+    // 계산 결과를 담을 변수를 준비한다.
+    int result = 0;
+
+    // 메서드를 호출하여 작업을 수행하고,
+    // 리턴 결과는 로컬 변수에 저장한다.
+    result = plus(2, 3);
+    result = minus(result, 1);
+    result = multiple(result, 7);
+    result = divide(result, 3);
+
+    System.out.printf("result = %d\n", result);
+  }
+
+  static int plus(int a, int b) {
+    return a + b;
+  }
+
+  static int minus(int a, int b) {
+    return a - b;
+  }
+
+  static int multiple(int a, int b) {
+    return a * b;
+  }
+
+  static int divide(int a, int b) {
+    return a / b;
+  }
+}
+// 클래스 문법의 용도?
+// 1) 사용자 정의 데이터 타입 만들 때
+// - 즉 새로운 구조의 메모리를 설계할 때 사용한다.
+// 2) 메서드를 묶을 때
+// - 서로 관련된 기능을 관리하기 쉽게 묶고 싶을 때 사용한다.
+
+
