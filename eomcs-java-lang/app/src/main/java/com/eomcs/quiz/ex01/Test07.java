5e5ed664d4cb8c0f3b879f7e87d9f7f436b1b459
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-26 09:42:34 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test07.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test07.java
@@ -1,12 +1,11 @@
 package com.eomcs.quiz.ex01;
 
 // 출처: codefights.com
-// 한 줄에 버그가 있다. 고쳐라! 
-
-/* n!(n 팩토리얼) 값을 조사하여 몇 개의 0으로 끝나는 지(trailing zeros) 개수를 리턴하라.
-예1) 10!의 값은 두 개의 0으로 끝난다.
-예2) 23!은 4개의 값으로 끝난다. (23!의 값은 4바이트를 초과하기 때문에 테스트하지 말아라!)
- */
+//
+// n!(n 팩토리얼) 값을 조사하여 몇 개의 0으로 끝나는 지(trailing zeros) 개수를 리턴하라.
+// 예1) 10!의 값은 두 개의 0으로 끝난다.
+// 예2) 23!은 4개의 값으로 끝난다. (23!의 값은 4바이트를 초과하기 때문에 테스트하지 말아라!)
+//
 /* 원문
 Given an integer n, find the number of trailing zeros 
 in the decimal representation of n! 
