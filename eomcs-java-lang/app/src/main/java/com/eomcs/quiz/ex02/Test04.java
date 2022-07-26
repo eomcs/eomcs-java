c625ed3431abadf2e37bde5b47fbc81c2e872f10
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-26 10:03:01 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex02/Test04.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex02/Test04.java
@@ -1,19 +1,18 @@
 package com.eomcs.quiz.ex02;
 
 // 출처: codefights.com
-// 한 줄에 버그가 있다. 고쳐라! 
-/*
-등비수열(geometric progression 또는 geometric sequence)인지 검사하라.
-등비수열이면 true, 아니면 flase를 출력하라.
-
-등비수열이란?
-=> 각 항이 그 앞 항과 일정한 비를 가지는 수열.
-=> 이 일정한 비를 공비(common ratio)라고 한다.
-
-Example
-isGeometricProgression([1, 4, 16]) = true   <--- 공비는 4
-isGeometricProgression([2, 4, 8, 17, 34]) = false
- */
+//
+// 등비수열(geometric progression 또는 geometric sequence)인지 검사하라.
+// 등비수열이면 true, 아니면 flase를 출력하라.
+//
+// 등비수열이란?
+// => 각 항이 그 앞 항과 일정한 비를 가지는 수열.
+// => 이 일정한 비를 공비(common ratio)라고 한다.
+//
+// Example
+// isGeometricProgression([1, 4, 16]) = true   <--- 공비는 4
+// isGeometricProgression([2, 4, 8, 17, 34]) = false
+//
 
 /* 원문
 A geometric progression is a sequence of numbers 
@@ -39,12 +38,18 @@ public class Test04 {
 
   public static void main(String[] args) {
     System.out.println(isGeometricProgression(new int[]{1,4,16}) == true);
+    System.out.println(isGeometricProgression(new int[]{2,6,18,54,162}) == true);
     System.out.println(isGeometricProgression(new int[]{2,4,8,17,34}) == false);
 
   }
 
   static boolean isGeometricProgression(int[] sequence) {
     // 이 메서드를 완성하시오!
+    for (int i = 2; i < sequence.length; i++) {
+      if (sequence[0] * sequence[i] != sequence[1] * sequence[i - 1]) {
+        return false;
+      }
+    }
     return true;
   }
 }
--- eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex02/sol/Test04.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex02/sol/Test04.java
@@ -1,19 +1,18 @@
 package com.eomcs.quiz.ex02.sol;
 
 // 출처: codefights.com
-// 한 줄에 버그가 있다. 고쳐라! 
-/*
-등비수열(geometric progression 또는 geometric sequence)인지 검사하라.
-등비수열이면 true, 아니면 flase를 출력하라.
-
-등비수열이란?
-=> 각 항이 그 앞 항과 일정한 비를 가지는 수열.
-=> 이 일정한 비를 공비(common ratio)라고 한다.
-
-Example
-isGeometricProgression([1, 4, 16]) = true   <--- 공비는 4
-isGeometricProgression([2, 4, 8, 17, 34]) = false
- */
+//
+// 등비수열(geometric progression 또는 geometric sequence)인지 검사하라.
+// 등비수열이면 true, 아니면 flase를 출력하라.
+//
+// 등비수열이란?
+// => 각 항이 그 앞 항과 일정한 비를 가지는 수열.
+// => 이 일정한 비를 공비(common ratio)라고 한다.
+//
+// Example
+// isGeometricProgression([1, 4, 16]) = true   <--- 공비는 4
+// isGeometricProgression([2, 4, 8, 17, 34]) = false
+//
 
 /* 원문
 A geometric progression is a sequence of numbers 
