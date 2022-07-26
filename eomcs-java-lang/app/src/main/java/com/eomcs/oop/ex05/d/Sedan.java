cc2f264a86f3c05040d0d96b7ac6d5df922fafc2
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-26 12:19:35 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex05/d/Sedan.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex05/d/Sedan.java
@@ -6,7 +6,7 @@ package com.eomcs.oop.ex05.d;
 // => 기존 클래스의 코드를 손대지 않고 기능을 확장하게 도와주는 문법이다.
 // => 재사용할 기존 클래스를 지정한다.
 // => 새 클래스에는 추가할 기능을 덧붙인다.
-public class Sedan extends Car {
+public class Sedan extends Car { // "Sedan을 통해서 Car의 기능을 사용할 수 있다" 
 
   // 인스턴스 변수 추가
   public boolean sunroof;
