534bb1c95a852dadcc286f73a3baebbb8d91c4bf
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-28 08:55:13 +0900

    .

---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test11.java
@@ -0,0 +1,34 @@
+package com.eomcs.quiz.ex01;
+
+// 출처: codefights.com
+//
+// 소문자 알파벳의 문자열이 주어질 때, 서로 다른 알파벳의 개수는?
+//
+// 예) "cabca" ==> 3
+//
+/*
+Given a string, find the number of different characters in it.
+
+for "cabca" output should be 3
+
+[input] string s
+
+a string of lowercase latin letters
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - ?
+//
+public class Test11 {
+
+  public static void main(String[] args) {
+    System.out.println(differentSymbolsNaive("cabca") == 3);
+  }
+
+  static int differentSymbolsNaive(String s) {
+    int result = 0;
+    // 이 메서드를 완성하시오!
+    return result;
+  }
+}
---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/sol/Test11.java
@@ -0,0 +1,47 @@
+package com.eomcs.quiz.ex01.sol;
+
+// 출처: codefights.com
+//
+// 소문자 알파벳의 문자열이 주어질 때, 서로 다른 알파벳의 개수는?
+//
+// 예) "cabca" ==> 3
+//
+/*
+Given a string, find the number of different characters in it.
+
+for "cabca" output should be 3
+
+[input] string s
+
+a string of lowercase latin letters
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - O(26n) = O(n) : n은 문자열의 개수
+//
+public class Test11 {
+
+  public static void main(String[] args) {
+    System.out.println(differentSymbolsNaive("cabca") == 3);
+  }
+
+  static int differentSymbolsNaive(String s) {
+
+    int result = 0;
+
+    for (int i = 0; i <= 26; i++) {
+      boolean found = false;
+      for (int j = 0; j < s.length(); j++) {
+        if (s.charAt(j) == 'a' + i) {
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        result++;
+      }
+    }
+    return result;
+  }
+}
