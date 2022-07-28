7e8c5269d96f3e3ad0edfff52c3d699b87be9ba6
eomjinyoung <jinyoung.eom@gmail.com>
2022-07-29 08:57:12 +0900

    .

---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test14.java
@@ -0,0 +1,38 @@
+package com.eomcs.quiz.ex01;
+
+// 출처: codefights.com
+//
+// 양의 정수 10진수를 꺼꾸로 만들어라.
+// 
+// 예) numberReverse(123) = 321
+//     numberReverse(120) = 21
+// 
+/*
+Reverse a given integer by rearranging its digits.
+
+Example
+
+numberReverse(123) = 321
+numberReverse(120) = 21
+
+[input] integer input
+positive integer
+
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - ?
+//
+public class Test14 {
+  public static void main(String[] args) {
+    System.out.println(numberReverse(123) == 321);
+    System.out.println(numberReverse(120) == 21);
+  }
+
+  static int numberReverse(int input) {
+    int reversed = 0;
+    // 이 메서드를 완성하시오!
+    return reversed;
+  }
+}
---
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex01/sol/Test13.java
@@ -0,0 +1,48 @@
+package com.eomcs.quiz.ex01.sol;
+
+//출처: codefights.com
+//
+// k의 배수 중에서 n보다 크지 않은 값을 모두 더한 결과는 무엇인가?
+// 조건:
+//   n과 k는 양의 정수이다.
+//
+// 예) sumOfMultiples(7, 2) = 2 + 4 + 6 = 12
+//   
+//
+/*
+For given n and k find the sum of all multiples of k 
+that are not greater than n.
+
+Example
+
+sumOfMultiples(7, 2) = 2 + 4 + 6 = 12
+
+[input] integer n
+positive integer
+
+[input] integer k
+positive integer
+
+[output] integer
+ */
+//
+// [시간 복잡도]
+// - O(n/k) = O(n) : n은 k의 배수들의 개수이다.
+//
+public class Test13 {
+  public static void main(String[] args) {
+    System.out.println(sumOfMultiples(7, 2) == 12);
+  }
+
+  static int sumOfMultiples(int n, int k) {
+    int result = 0;
+
+    for (int i = 1; i <= n; i++) {
+      if (i % k == 0) {
+        result += i;
+      }
+    }
+
+    return result;
+  }
+}
