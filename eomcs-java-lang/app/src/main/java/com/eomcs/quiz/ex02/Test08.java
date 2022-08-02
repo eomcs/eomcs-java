ba4b9ec63f191bcfe4ccf198e11e748c546e8617
eomjinyoung <jinyoung.eom@gmail.com>
2022-08-02 09:05:14 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex02/Test08.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/quiz/ex02/Test08.java
@@ -1,7 +1,12 @@
-package com.eomcs.algorithm.quiz;
+package com.eomcs.quiz.ex02;
 // 출처: codefights.com
 // 
-//
+// 장애물들의 x 좌표(양의 좌표)가 배열로 주어질 때,
+// 0부터 오른쪽으로 동일한 거리를 점프하여 장애물들을 피한다면,
+// 점프해야 할 최소 거리는 얼마인지 구하라!
+// 
+// 예) 
+//   [5, 3, 6, 7, 9] ==> 4
 // 
 /*
 You are given an array of integers representing coordinates of obstacles 
@@ -18,29 +23,23 @@ Example
 for [5, 3, 6, 7, 9] output should be 4
 
 [input] array.integer inputArray
-
 non-empty array of positive integers
-[output] integer
 
+[output] integer
 the desired length
  */
-public class Test019 {
-  static int avoidObstacles(int[] inputArray) {
+//
+// [시간 복잡도]
+// - ?
+//
+public class Test08 {
 
-    for (int i = 1; ; i++) {
-      for (int j = 0; j < inputArray.length; j++) {
-        if (inputArray[j] % i == 0) {
-          break;
-        }
-        if (j == inputArray.length - 1) {
-          return i;
-        }
-      }
-    }  
-  }
   public static void main(String[] args) {
-    System.out.println(avoidObstacles(new int[]{5,3,6,7,9}));
-
+    System.out.println(avoidObstacles(new int[]{5,3,6,7,9,13,11}) == 4);
   }
 
+  static int avoidObstacles(int[] inputArray) {
+    // 이 메서드를 완성하시오!
+    return 0;
+  }
 }
