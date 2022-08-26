853f3be360932778a6cc5799c67a17b47ba8a90e
eomjinyoung <jinyoung.eom@gmail.com>
2022-08-26 14:40:49 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/concurrent/ex5/Exam0310.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/concurrent/ex5/Exam0310.java
@@ -15,7 +15,7 @@ public class Exam0310 {
   static void print1(String threadName, Counter counter) {
     System.out.printf("[%s] 출력 시작 ----------\n", threadName);
 
-    for (int i  = 0; i < counter.value; i++) {
+    for (int i= 0; i < counter.value; i++) {
       System.out.printf("%s ==> %d\n", threadName, i);
     }
 
@@ -66,9 +66,9 @@ public class Exam0310 {
   public static void main(String[] args) throws Exception {
     Counter counter = new Counter(1000);
 
-    Worker w1 = new Worker("홍길동", counter);
-    Worker w2 = new Worker("%%%%%%%", counter);
-    Worker w3 = new Worker("kim", counter);
+    Worker w1 = new Worker("**홍길동", counter);
+    Worker w2 = new Worker("임꺽정---->", counter);
+    Worker w3 = new Worker("유%관%순", counter);
 
     w1.start();
     w2.start();
