75ad09452452c1c65b8ab0f50376803249467635
eomjinyoung <jinyoung.eom@gmail.com>
2022-10-24 16:33:11 +0900

    .

--- eomcs-web/app/src/main/java/com/eomcs/web/App.java
+++ eomcs-web/app/src/main/java/com/eomcs/web/App.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class App {
 
   public static void main(String[] args) {
+    System.out.println("서버 시작!");
     SpringApplication.run(App.class, args);
   }
 
