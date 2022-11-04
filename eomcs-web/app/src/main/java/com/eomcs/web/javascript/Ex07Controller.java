3e0cc95952d4157c81d9952d6ad87678d00c4c87
eomjinyoung <jinyoung.eom@gmail.com>
2022-11-04 18:17:50 +0900

    .

--- eomcs-web/app/src/main/java/com/eomcs/web/javascript/Ex07Controller.java
+++ eomcs-web/app/src/main/java/com/eomcs/web/javascript/Ex07Controller.java
@@ -1,11 +1,22 @@
 package com.eomcs.web.javascript;
 
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class Ex07Controller {
+
+  // 외부 사이트로 HTTP 요청을 수행하는 도구
+  RestTemplate restTemplate;
+
+  public Ex07Controller(RestTemplateBuilder restTemplateBuilder) {
+    restTemplate = restTemplateBuilder.build();
+  }
 
   @RequestMapping("/javascript/ex07/test1")
   public void test1() {
@@ -21,4 +32,33 @@ public class Ex07Controller {
     // HttpServletResponse를 사용한다면 view URL을 명시적으로 지정해야 한다.
     return "/javascript/ex07/test2";
   }
+
+  @ResponseBody
+  @RequestMapping("/javascript/ex07/test3")
+  public String test3(String url) {
+    return restTemplate.getForObject(url, String.class);
+  }
+
+  @RequestMapping("/javascript/ex07/test4")
+  public void test4(String name, int age, Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("age", age);
+  }
+
+  @RequestMapping("/javascript/ex07/test5")
+  public void test5() throws Exception {
+    Thread.sleep(2000);
+  }
+
+  @RequestMapping("/javascript/ex07/test6")
+  public void test6(int a, int b, Model model) throws Exception {
+    model.addAttribute("result", a + b);
+  }
+
 }
+
+
+
+
+
+
