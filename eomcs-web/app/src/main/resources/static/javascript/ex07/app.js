98a47b5ffb9c32f4a2b5902519cc96715e9af1a0
eomjinyoung <jinyoung.eom@gmail.com>
2022-12-16 18:54:48 +0900

    .

--- eomcs-web/app/src/main/resources/static/javascript/ex07/app.js
+++ eomcs-web/app/src/main/resources/static/javascript/ex07/app.js
@@ -9,6 +9,11 @@ const port = 3000; // 웹서버 포트 번호
 // express()를 호출하여 웹서버를 준비한다.
 const app = express();
 
+// POST 요청으로 보낸 payload 데이터를 분석할 객체를 지정하기
+// => Content-Type: application/x-www-form-urlencoded 형식으로 된 payload 처리
+//    예) name=hong&age=20
+app.use(express.urlencoded({extended: true}));
+
 // 클라이언트 요청에 대해 호출될 메서드를 등록
 app.get(                 // GET 요청이 들어 왔을 때 호출될 메서드 지정
   '/exam01-1',           // 요청 URL
@@ -33,10 +38,61 @@ app.post('/exam02-2', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Content-Type', 'text/plain; charset=UTF-8');
 
-  var payload = `이름: ${req.query.name}\n`;
-  payload += `나이: ${req.query.age}\n`;
+  var payload = `이름: ${req.body.name}\n`;
+  payload += `나이: ${req.body.age}\n`;
 
   res.send(payload);
+}); 
+
+app.get('/exam03-1', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Content-Type', 'text/plain; charset=UTF-8');
+
+  setTimeout(()=> {
+    res.send("Hello!");
+  }, 10000);
+  
+}); 
+
+app.get('/exam03-4', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Content-Type', 'text/plain; charset=UTF-8');
+
+  let a = parseInt(req.query.a);
+  let b = parseInt(req.query.b);
+
+  res.send(`${a + b}`);
+}); 
+
+app.get('/header', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Content-Type', 'text/html; charset=UTF-8');
+
+  res.send('<h1>비트캠프 네이버 클라우드 AIaaS 개발자 양성과정</h1>');
+}); 
+
+app.get('/footer', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Content-Type', 'text/html; charset=UTF-8');
+
+  res.send('<address>비트캠프 서초캠프@2022</address>');
+}); 
+
+app.get('/exam04-3', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Content-Type', 'text/html; charset=UTF-8');
+
+  let arr = [
+    {no:1,title:'제목1',writer:'홍길동',viewCnt:19},
+    {no:2,title:'제목2',writer:'임꺽정',viewCnt:312},
+    {no:3,title:'제목3',writer:'유관순',viewCnt:31},
+    {no:4,title:'제목4',writer:'안중근',viewCnt:100},
+    {no:5,title:'제목5',writer:'윤봉길',viewCnt:200}
+  ];
+
+  // 배열 객체를 JSON 문자열로 변환하여 클라이언트에게 보낸다.
+  // => serialization(직렬화)
+  res.send(JSON.stringify(arr));
 }); 
 
 // 클라이언트 요청을 다른 서버에게 보낸다.
@@ -47,6 +103,28 @@ app.get('/proxy', (req, res) => {
 
   request.get({
       uri: req.query.url
+    }, (error, response, body) => {
+      res.send(body);
+  });
+}); 
+
+app.get('/proxy2', (req, res) => {    
+
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Content-Type', 'application/json; charset=UTF-8');
+
+  let openApiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?" +
+    "serviceKey=swGnUeGbwJTEP%2FoOuEZsMstoBza7CSMZhJ%2FQGdasAbhNX5oUrxUn8sSJ3YjY3H0Og39RlBLPXtKWmxqRKjHAwQ%3D%3D" + 
+    "&pageNo=1" + 
+    "&numOfRows=1000" + 
+    "&dataType=JSON" + 
+    "&base_date=" + req.query.base_date +
+    "&base_time=0600" + 
+    "&nx=" + req.query.nx +  
+    "&ny=" + req.query.ny;
+
+    request.get({
+      uri: openApiUrl
     }, (error, response, body) => {
       res.send(body);
   });
