633720baddc7d2d4953be853a51fe6e4380a1227
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-02 16:44:28 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0251.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0251.java
@@ -4,7 +4,7 @@ package com.eomcs.lang.ex07;
 //
 public class Exam0251 {
 
-  // 가변 파라미터에 배열을 넘길 경우 
+  // 가변 파라미터에 배열을 넘길 경우
   // 기존 배열을 그대로 사용할까? 아니면 파라미터로 받은 배열을 복제해서 사용할까?
   // => 가변 파라미터에 배열을 넘길 경우 그 배열을 그대로 받아 사용한다.
   static void hello(String... names) {
@@ -16,7 +16,7 @@ public class Exam0251 {
 
   public static void main(String[] args) {
 
-    String[] arr = {"김구", "안중근", "윤봉길", "유관순"};
+    String[] arr = { "김구", "안중근", "윤봉길", "유관순" };
 
     // 가변 파라미터에 배열을 넘길 경우
     hello(arr);
