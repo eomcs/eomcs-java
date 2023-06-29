2fd12203cd04bb428fe02ff3c38d0f0dab62c138
eomjinyoung <jinyoung.eom@gmail.com>
2023-06-29 11:35:55 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/io/ex06/Exam0110.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/io/ex06/Exam0110.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 public class Exam0110 {
 
   public static void main(String[] args) throws Exception {
-    FileInputStream in = new FileInputStream("temp/jls11.pdf");
+    FileInputStream in = new FileInputStream("temp/jls17.pdf");
 
     int b;
 
