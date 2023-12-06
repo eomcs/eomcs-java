f9cde546c81acc6ffad2d49e1c6c8eb8a3837e0a
eomjinyoung <jinyoung.eom@gmail.com>
2023-12-06 13:25:48 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex02/step18/vo/Score.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/oop/ex02/step18/vo/Score.java
@@ -25,7 +25,7 @@ public class Score {
     this.eng = eng;
     this.math = math;
 
-    this.compute();
+    this.compute(); // 컴파일 할 때 컴파일러가 compute() 앞에 생성자가 받은 인스턴스 주소를 넘깁니다.
   }
 
   public void compute() {
