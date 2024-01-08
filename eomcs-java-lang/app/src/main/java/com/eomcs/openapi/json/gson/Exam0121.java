4a9de479e6e1c257b64c7a33080bf96af2473991
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-08 15:26:00 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/openapi/json/gson/Exam0121.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/openapi/json/gson/Exam0121.java
@@ -11,6 +11,7 @@ public class Exam0121 {
     String jsonStr = "{\"no\":100,\"name\":\"홍길동\",\"email\":\"hong@test.com\",\"password\":\"1111\",\"photo\":\"hong.gif\",\"tel\":\"010-2222-1111\",\"registeredDate\":\"2023-07-03\"}";
 
     // 2) JSON 처리 객체 준비
+    //    Gson gson = new Gson(); // 실행 오류! yyyy-MM-dd 형식으로 된 날짜를 인식하지 못해서 오류 발생.
     Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 
     // 3) JSON 문자열을 가지고 객체 만들기
