46fe92901c50f41a1cf0a348164adc31f1757311
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-24 12:30:08 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/concurrent/ex4/Exam0220.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/concurrent/ex4/Exam0220.java
@@ -14,15 +14,16 @@ public class Exam0220 {
         for (int i = 0; i < 100000000; i++)
           Math.asin(38.567); // 시간 끌기 용. 왜? 부동소수점 연산은 시간을 많이 소요.
         long endTime = System.currentTimeMillis();
-        System.out.printf("MyThread = %d\n", endTime - startTime);
+        System.out.printf("%s 스레드 실행 시간: %d\n", getName(), endTime - startTime);
       }
     }
 
-    // main 스레드의 우선 순위를 가장 작은 1로 설정한다.
-    Thread.currentThread().setPriority(1);
+    MyThread t1 = new MyThread("홍길동(1)");
+    t1.setPriority(1);
 
-    MyThread t1 = new MyThread("t1");
-    t1.setPriority(10);
+    MyThread t2 = new MyThread("임꺽정(10)");
+    t2.setPriority(10);
+
     // 유닉스 계열의 OS는 스케줄링에서 우선 순위를 고려하여 CPU를 배분한다.
     // 그러나 Windows OS는 우선 순위를 덜 고려하여 CPU를 배분한다.
     // 그러다보니 우선 순위를 조정하여 작업을 처리하도록 프로그램을 짜게 되면,
@@ -30,20 +31,10 @@ public class Exam0220 {
     // 윈도우에서는 의도대로 동작하지 않을 것이다.
     // 따라서 프로그램을 짤 때 스레드의 우선 순위를 조정하는 방법에 의존하지 말라!
 
-    System.out.printf("main 스레드 우선 순위: %d\n", Thread.currentThread().getPriority());
-
-    System.out.printf("%s 스레드 우선 순위: %d\n", t1.getName(), t1.getPriority());
-
-
-    // t1 스레드 작업 시작
+    // 스레드 작업 시작
     t1.start();
+    t2.start();
 
-    // main 스레드 작업 시작
-    long startTime = System.currentTimeMillis();
-    for (int i = 0; i < 100000000; i++)
-      Math.asin(38.567); // 부동 소수점 연산을 수행하는 코드를 넣어서 실행 시간을 약간 지연시킨다.
-    long endTime = System.currentTimeMillis();
-    System.out.printf("main = %d\n", endTime - startTime);
   }
 
 }
