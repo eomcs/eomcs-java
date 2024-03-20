8ccb953e870fdb100c27fd630590d5d5cdf4b3d5
eomjinyoung <jinyoung.eom@gmail.com>
2024-03-20 17:07:55 +0900

    .

--- eomcs-spring-webmvc/app/src-14/main/java/bitcamp/app2/Controller01_1.java
+++ eomcs-spring-webmvc/app/src-14/main/java/bitcamp/app2/Controller01_1.java
@@ -21,16 +21,18 @@ public class Controller01_1 {
     model.addAttribute("age", 20);
 
     return "/jsp/c01_1.jsp";
-    // 기본 ViewResolver는 리턴 값으로 받은 view name으로 JSP를 찾는다.
-    // 1) 절대 경로로 시작할 때 - '/' 로 시작할 때 
+    // 기본 ViewResolver는 리턴 값으로 받은 view name으로 포워딩 할 JSP를 찾는다.
+    // 1) 절대 경로로 시작할 때('/' 로 시작할 때)
     //    예) return "/jsp/c01_1.jsp"
-    //        view URL = /웹애플리케이션루트경로(/) + view name
+    //        view URL = 현재웹애플리케이션경로 + view name
+    //                 = / + /jsp/c01_1.jsp
     //                 = /jsp/c01_1.jsp
     // 
     // 2) 상대 경로로 시작할 때 - '/' 로 시작하지 않을 때
     //    예) return "jsp/c01_1.jsp"
     //        view URL = 현재 URL 경로 + view name
-    //                 = /웹애플리케이션경로/app2/c01_1 + jsp/c01_1.jsp
+    //                 = /현재웹애플리케이션경로 + request handler 경로 + jsp/c01_1.jsp
+    //                 = / + /app2/c01_1/ + jsp/c01_1.jsp
     //                 = /app2/c01_1/jsp/c01_1.jsp
     // 
     //
@@ -46,12 +48,12 @@ public class Controller01_1 {
     model.addAttribute("age", 30);
 
     // 뷰 이름을 리턴하지 않으면, 
-    // request handler의 URL을 상대 경로 view name으로 사용한다.
+    // request handler의 URL을 상대 경로로 사용한다.
     // 즉 다음 리턴 문과 같다.
     //   return "c01_1/h2"
     // 계산 방법:
-    // => view URL = 현재 URL 경로 + view name
-    //             = /웹애플리케이션경로/app2/c01_1 + c01_1/h2
+    // => view URL = 현재 URL 경로(현재웹애플리케이션경로 + request handler 경로) + view name
+    //             = / + /app2/c01_1 + c01_1/h2
     //             = /app2/c01_1/c01_1/h2
     //    따라서 잘못 계산된 view URL로 JSP를 찾으니까 오류가 발생한다!
     // 
