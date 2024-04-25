f33652ad9d2052b0f6253407c8265ef36bd47a15
eomjinyoung <jinyoung.eom@gmail.com>
2024-04-25 10:29:16 +0900

    .

--- eomcs-web/app/src/main/webapp/javascript/ex07/myQuery.js
+++ eomcs-web/app/src/main/webapp/javascript/ex07/myQuery.js
@@ -24,5 +24,66 @@ function myQuery(p) {
   return e;
 }
 
+myQuery.ajax = function(p1, p2) {
+  let url, settings;
+  settings = {};
+
+  if ('string' == typeof p1) {
+    url = p1;
+    settings = p2;
+  } else {
+    settings = p1;
+    url = settings.url;
+  }
+
+  if (settings.method == undefined) {
+    settings.method = 'get';
+  }
+
+  if (settings.dataType == undefined) {
+    settings.dataType = 'text';
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          if (settings.success) {
+            if (settings.dataType == 'json') {
+              settings.success(JSON.parse(xhr.responseText))
+            } else {
+              settings.success(xhr.responseText);
+            }
+          }
+        } else {
+          if (settings.error) {
+            settings.error(xhr.responseText);
+          }
+        }
+      }
+  };
+
+  let arr = [];
+  if (settings.data) {
+    for (let name in settings.data) {
+      arr.push(name + '=' + settings.data[name])
+    }
+  }
+  let qs = arr.join('&');
+
+  if (settings.method == 'get') {
+    xhr.open(settings.method, url + '?' + qs, true);
+    xhr.send();
+
+  } else if (settings.method == 'post') {
+    xhr.open(settings.method, url, true);
+    xhr.setRequestHeader(
+          "Content-Type",
+          "application/x-www-form-urlencoded");
+    xhr.send(qs);
+  }
+};
+
+
 
 let $ = myQuery;
--- eomcs-web/app/src/main/webapp/javascript/ex07/test4.jsp
+++ eomcs-web/app/src/main/webapp/javascript/ex07/test4.jsp
@@ -1,8 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <% response.setHeader("Access-Control-Allow-Origin", "*");%>
-${Integer.parseInt(param.a) + Integer.parseInt(param.b)}
-
+{
+  "status": "success",
+  "data": ${Integer.parseInt(param.a) + Integer.parseInt(param.b)}
+}
 
 
 
