f7b74e0234166aa63911d9e9fac7d75c1c5e4831
eomjinyoung <jinyoung.eom@gmail.com>
2024-06-11 12:45:48 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex04/Exam0510.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex04/Exam0510.java
@@ -1,6 +1,6 @@
 package com.eomcs.lang.ex04;
 
-//# 배열 - 배열을 선언하는 방법 
+//# 배열 - 배열을 선언하는 방법
 // 배열?
 // - 같은 종류의 메모리를 쉽게 만드는 방법
 //
@@ -9,12 +9,12 @@ package com.eomcs.lang.ex04;
 //        데이터타입[] 변수명 = new 데이터타입[개수];
 //        ex) int[] arr = new int[5];
 //
-// - C언어 스타일 
+// - C언어 스타일
 //        데이터타입 변수명[] = new 데이터타입[개수];
 //        ex) int arr[] = new int[5];
 //
 // 배열의 개수는 (int 타입의 최대 값 - 2)와 같다.
-// 즉 2147483645 개 이다. 
+// 즉 2147483645 개 이다.
 //
 public class Exam0510 {
   public static void main(String[] args) {
@@ -28,7 +28,7 @@ public class Exam0510 {
     // 배열의 크기는 int 타입의 최대 값에서 2를 뺀 값이다.
     // 배열의 최대 크기 = Integer.MAX_VALUE - 2
 
-    // int[] arr3 = new int[2147483645]; // 실행 오류 => VM의 배열 크기 제한을 초과
+    int[] arr3 = new int[2147483645]; // 실행 오류 => VM의 배열 크기 제한을 초과
     // Exception in thread "main" java.lang.OutOfMemoryError: Requested array size
     // exceeds VM limit
 
