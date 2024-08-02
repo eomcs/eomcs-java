a8498bb8a99890d1b7765755b8b66a8d7571e7a8
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-02 15:46:52 +0900

    .

--- eomcs-java-lang/app/src/main/java/com/eomcs/concurrent/ex4/Exam0110.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/concurrent/ex4/Exam0110.java
@@ -5,7 +5,7 @@ public class Exam0110 {
   public static void main(String[] args) {
     // 스레드의 생명주기
     // new Thread()    start()              sleep()/wait()
-    //     준비 -------------------> Running ---------------> Not Runnable
+    //     준비 -------------------> Running --------------->
     //                               ^  |    <---------------
     //                               |  |    timeout/notify()
     //                               X  |
@@ -17,10 +17,10 @@ public class Exam0110 {
     //
     // Not Runnable 상태?
     // - CPU를 받지 않는 상태
-    // 
-    // run() 메서드 종료 후 다시 running 상태로 돌아갈 수 없다. 
+    //
+    // run() 메서드 종료 후 다시 running 상태로 돌아갈 수 없다.
     // => 새로 스레드를 만들어 실행하는 방법 밖에 없다!
-    // 
+    //
     System.out.println("스레드 실행 전");
     new Thread() {
       @Override
@@ -32,7 +32,7 @@ public class Exam0110 {
     }.start();
 
     System.out.println("스레드 실행 후");
-    // main() 메서드의 호출이 끝나더라도 다른 스레드의 실행이 종료될 때까지 
+    // main() 메서드의 호출이 끝나더라도 다른 스레드의 실행이 종료될 때까지
     // JVM은 종료하지 않는다.
   }
 
